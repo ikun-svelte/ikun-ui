fbd5443a45281bd74aeee417ada4e93a9014b24a
白雾三语 <32354856+baiwusanyu-c@users.noreply.github.com>
2023-08-03 21:01:37 +0800

    chore: temp commit

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,15 +1,15 @@
 import theme from 'vitepress/theme';
 import { h } from 'vue';
-// import DemoBlock from '../components/demo-block';
-// import { highlight } from '../components/highlight';
+import DemoBlock from '../components/demo-block';
+import { highlight } from '../components/highlight';
 import './style.css';
 import 'uno.css';
 
 export default {
 	...theme,
 	enhanceApp({ app }) {
-		// app.component('Demo', DemoBlock);
-		// app.component('highlight', highlight);
+		app.component('Demo', DemoBlock);
+		app.component('highlight', highlight);
 	},
 	Layout() {
 		return h(theme.Layout);
