c4f21ed6f9d6d99187a16a1f0cd591ff30686ca2
soya <3100034521@qq.com>
2023-08-07 10:12:20 +0800

    chore: input & select icon error & format code

--- play/src/routes/+page.svelte
+++ play/src/routes/+page.svelte
@@ -1,118 +1,84 @@
 <script lang="ts">
-import { KButton } from '@ikun-ui/button'
-import { KIcon } from '@ikun-ui/icon'
-import { KCollapse } from '@ikun-ui/collapse'
-import { KCheckbox } from '@ikun-ui/checkbox'
-import { KDrawer } from '@ikun-ui/drawer'
-import { KEyeDropper } from '@ikun-ui/eye-dropper'
-import { KInput } from '@ikun-ui/input'
-import { KPopover } from '@ikun-ui/popover'
-import { KTooltip } from '@ikun-ui/tooltip'
-import { KModal } from '@ikun-ui/modal'
-import { KTag } from '@ikun-ui/tag'
-import { KNotify } from '@ikun-ui/notify'
-import { KSwitch } from '@ikun-ui/switch'
-import { KSelect } from '@ikun-ui/select'
-import 'virtual:uno.css'
+	import { KButton } from '@ikun-ui/button';
+	import { KIcon } from '@ikun-ui/icon';
+	import { KCollapse } from '@ikun-ui/collapse';
+	import { KCheckbox } from '@ikun-ui/checkbox';
+	import { KDrawer } from '@ikun-ui/drawer';
+	import { KEyeDropper } from '@ikun-ui/eye-dropper';
+	import { KInput } from '@ikun-ui/input';
+	import { KPopover } from '@ikun-ui/popover';
+	import { KTooltip } from '@ikun-ui/tooltip';
+	import { KModal } from '@ikun-ui/modal';
+	import { KTag } from '@ikun-ui/tag';
+	import { KNotify } from '@ikun-ui/notify';
+	import { KSwitch } from '@ikun-ui/switch';
+	import { KSelect } from '@ikun-ui/select';
+	import 'virtual:uno.css';
 
-let checked = true
-let openDrawer = false
-let openModal = false
-let inputValue = ''
+	let checked = true;
+	let openDrawer = false;
+	let openModal = false;
+	let inputValue = '';
 
-const handleNotify = () => {
-  KNotify.success({
-    title: 'Notify Success',
-    duration: 99999,
-    autoClose: true,
-    content: 'Notify Success'
-  })
-}
+	const handleNotify = () => {
+		KNotify.success({
+			title: 'Notify Success',
+			duration: 99999,
+			autoClose: true,
+			content: 'Notify Success'
+		});
+	};
 
-let switchVal = false
-let selectVal = ''
+	let switchVal = false;
+	let selectVal = '';
 </script>
-<KButton icon="i-carbon-settings"
-         type="primary"
-         on:click={() => openDrawer = true}>
-    drawer
+
+<KButton icon="i-carbon-settings" type="primary" on:click={() => (openDrawer = true)}>
+	drawer
 </KButton>
-<KIcon icon="i-carbon-settings" btn ></KIcon>
+<KIcon icon="i-carbon-settings" btn></KIcon>
 <KCollapse>
-    <span slot="title">title</span>
-    <span slot="content">content</span>
+	<span slot="title">title</span>
+	<span slot="content">content</span>
 </KCollapse>
-<KCheckbox value={checked}
-           on:updateValue={(e) => checked = e.detail}
-           label={`${checked}`}>
-</KCheckbox>
-<KDrawer value={openDrawer}
-         on:close={() => openDrawer = false}>
-    KDrawer
-</KDrawer>
+<KCheckbox value={checked} on:updateValue={(e) => (checked = e.detail)} label={`${checked}`}
+></KCheckbox>
+<KDrawer value={openDrawer} on:close={() => (openDrawer = false)}>KDrawer</KDrawer>
 <KEyeDropper let:open>
-    <KButton type="primary"
-             on:click={() => open()}>
-        KEyeDropper
-    </KButton>
+	<KButton type="primary" on:click={() => open()}>KEyeDropper</KButton>
 </KEyeDropper>
 
-<KInput value={inputValue}
-        placeholder="inputValue"
-        icon="i-carbon-settings">
-</KInput>
+<KInput value={inputValue} placeholder="inputValue" iconPrefix="i-carbon-settings"></KInput>
 
 <KTooltip content="KTooltip - content" placement="bottom">
-    <KButton type="warning"
-             slot="triggerEl">
-        KTooltip - trigger
-    </KButton>
+	<KButton type="warning" slot="triggerEl">KTooltip - trigger</KButton>
 </KTooltip>
 
 <KPopover placement="bottom">
-    <span slot="contentEl">KPopover - content</span>
-    <KButton type="error"
-             slot="triggerEl">
-        KPopover - trigger
-    </KButton>
+	<span slot="contentEl">KPopover - content</span>
+	<KButton type="error" slot="triggerEl">KPopover - trigger</KButton>
 </KPopover>
 
-<KButton icon="i-carbon-settings"
-         type="success"
-         on:click={() => openModal = true}>
-    KModal
+<KButton icon="i-carbon-settings" type="success" on:click={() => (openModal = true)}>
+	KModal
 </KButton>
 
-<KModal show={openModal} title="KModal" on:close={(v) => openModal = v.detail}>
-   <span>KModal</span>
+<KModal show={openModal} title="KModal" on:close={(v) => (openModal = v.detail)}>
+	<span>KModal</span>
 </KModal>
 
-<KTag type="success">
-    success KTag
-</KTag>
-<KTag type="info">
-    info KTag
-</KTag>
-<KTag type="primary">
-    primary KTag
-</KTag>
-<KTag type="error">
-    error KTag
-</KTag>
-<KTag type="warning">
-    warning KTag
-</KTag>
+<KTag type="success">success KTag</KTag>
+<KTag type="info">info KTag</KTag>
+<KTag type="primary">primary KTag</KTag>
+<KTag type="error">error KTag</KTag>
+<KTag type="warning">warning KTag</KTag>
 
 <KButton on:click={handleNotify}>handleNotify</KButton>
 
-<KSwitch value={switchVal}
-        on:updateValue={(v) =>switchVal= v.detail}>
-</KSwitch>
+<KSwitch value={switchVal} on:updateValue={(v) => (switchVal = v.detail)}></KSwitch>
 
-<KSelect value="{selectVal}"
-         icon="i-carbon-settings"
-         placeholder="selectValue">
-    {#each ['Tiny', 'Small', 'Normal', 'Large', 'Huge'] as item (item)}
-        <option>{item}</option>
-    {/each}
+<KSelect value={selectVal} iconPrefix="i-carbon-settings" placeholder="selectValue">
+	{#each ['Tiny', 'Small', 'Normal', 'Large', 'Huge'] as item (item)}
+		<option>{item}</option>
+	{/each}
 </KSelect>
