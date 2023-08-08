03036018f2ee6435b1120065e52ed67077d58a78
白雾三语 <32354856+baiwusanyu-c@users.noreply.github.com>
2023-08-08 09:22:56 +0800

    docs: added homepage logo animation (#48)
    * docs: added homepage logo animation
    
    * chore: format code

---
+++ docs/.vitepress/components/logo-animate/src/black.vue
@@ -0,0 +1,75 @@
+<template>
+	<section class="logo-black">
+		<span></span>
+	</section>
+</template>
+<style>
+.logo-black {
+	width: 84px;
+	height: 84px;
+	background: #000;
+	position: relative;
+	transform: rotate(45deg);
+	overflow: hidden;
+	clip-path: polygon(33% 0, 100% 0, 100% 100%, 0 100%, 0 33%, 33% 33%, 33% 0);
+	margin: -5px auto auto;
+	animation: logo-black var(--t) infinite var(--play-status);
+}
+.logo-black::before,
+.logo-black::after {
+	content: '';
+	position: absolute;
+	background-color: #fff;
+	width: 10px;
+	height: 100%;
+}
+.logo-black::before {
+	top: 25%;
+	transform: rotate(-45deg);
+}
+.logo-black::after {
+	top: -25%;
+	right: 0;
+	transform: rotate(-45deg);
+}
+.logo-black span {
+	position: absolute;
+	top: 25%;
+	left: 25%;
+	width: 100%;
+	height: 100%;
+	background: linear-gradient(to bottom, #fff 50%, #000 50%) 50% 0 / 10% 20% repeat-y;
+	transform: rotate(-45deg);
+}
+@keyframes logo-black {
+	0%,
+	10%,
+	90%,
+	100% {
+		transform: translateY(0) rotate(45deg);
+	}
+	35% {
+		transform: translate(10px, -10px) rotate(45deg);
+	}
+	40% {
+		transform: translate(0, 0) rotate(45deg);
+	}
+	50% {
+		transform: translate(10px, 10px) rotate(45deg);
+	}
+	65% {
+		transform: translate(0, 0) rotate(45deg);
+	}
+	70% {
+		transform: translate(10px, -10px) rotate(45deg);
+	}
+	75% {
+		transform: translate(10px, 10px) rotate(45deg);
+	}
+	20%,
+	30%,
+	80% {
+		transform: translateY(5px) rotate(45deg);
+	}
+}
+</style>
---
+++ docs/.vitepress/components/logo-animate/src/circle.vue
@@ -0,0 +1,77 @@
+<template>
+	<section class="logo-circle"></section>
+	<section class="shadow"></section>
+</template>
+<style>
+.logo-circle {
+	width: 100%;
+	height: 130px;
+	position: absolute;
+	top: 0;
+	left: 0;
+	animation: logo-circle var(--t) infinite var(--play-status);
+}
+.logo-circle::after {
+	content: '';
+	position: absolute;
+	width: 30px;
+	height: 30px;
+	background-color: #f80;
+	border-radius: 50%;
+	bottom: 0;
+	left: 0;
+	animation: play 1s infinite;
+}
+.shadow {
+	position: absolute;
+	bottom: -30px;
+	left: 0;
+	width: 30px;
+	height: 10px;
+	border-radius: 50%;
+	background: radial-gradient(#666, transparent 50%);
+	animation: play-shadow 1s infinite;
+	transform: scale(0.5);
+}
+.play .logo-circle::after {
+	animation-play-state: paused;
+}
+.play .shadow {
+	display: none;
+}
+@keyframes logo-circle {
+	25% {
+		transform: rotate(-60deg);
+	}
+	50% {
+		transform: rotate(-90deg);
+	}
+	75% {
+		transform: rotate(-30deg);
+	}
+}
+@keyframes play {
+	40% {
+		transform: translateY(20px);
+	}
+}
+@keyframes play-shadow {
+	40% {
+		transform: scale(1);
+	}
+}
+@keyframes run-shadow {
+	25% {
+		left: 78px;
+		transform: scale(1) translateY(10px);
+	}
+	50% {
+		left: 118px;
+		transform: scale(0.5);
+	}
+	75% {
+		left: 40px;
+		transform: scale(1) translateY(10px);
+	}
+}
+</style>
