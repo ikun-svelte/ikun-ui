c84c1a0e56bad93d88fb497525ec00961704dbda
moushicheng <moushicheng@outlook.com>
2023-08-15 00:40:05 +0800

    feat: support watch script

---
+++ scripts/watch.js
@@ -0,0 +1,19 @@
+import { runTask } from './utils.js';
+import chokidar from 'chokidar';
+
+function getComponentName(path) {
+	return path.split('\\')[1];
+}
+
+chokidar
+	.watch('./components', {
+		ignored: /(dist)|(node_modules)/ // ignore dotfiles
+	})
+	.on('ready', () => console.log('Initial scan complete. Ready for changes'))
+	.on('change', (path) => {
+		const component = getComponentName(path);
+		console.log(`${component} change,noew rebuilding.....`);
+		runTask(`npm run build:js && npm run build:svelte`, `components/${component}`, 'watch', {
+			silent: false
+		});
+	});
