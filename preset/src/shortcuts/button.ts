a86392b9b583fce58db3d119bd015d0d3bc2a325
WangYeWei <826036140@qq.com>
2023-08-14 17:57:25 +0800

    chore(button): add disabled styles

--- preset/src/shortcuts/button.ts
+++ preset/src/shortcuts/button.ts
@@ -1,37 +1,44 @@
 export const buttonShortcuts: Record<string, string> = {
 	// button
+	'k-button--disabled': 'opacity-50',
+
 	'k-button--base': 'rounded k-border cursor-pointer f-c px-2 py-1 text-base w-fit',
 	'k-button--circle': '!rounded-full p-2',
 
 	'k-button--primary':
-		'bg-ikun-primary-100 text-ikun-primary border-ikun-primary-100 hover:border-ikun-primary',
+		'bg-ikun-primary-100 text-ikun-primary border-ikun-primary-100',
 	'k-button--primary__active': 'active:border-ikun-primary active:k-btn-shadow--primary',
 	'k-button--primary__focus':
 		'focus-visible:border-ikun-primary focus-visible:k-btn-shadow--primary',
+	'k-button--primary__hover': 'hover:border-ikun-primary',
 	'k-button--primary__icon': 'text-ikun-primary',
 
 	'k-button--success':
-		'bg-ikun-success-100 text-ikun-success border-ikun-success-100 hover:border-ikun-success',
+		'bg-ikun-success-100 text-ikun-success border-ikun-success-100',
 	'k-button--success__active': 'active:border-ikun-success active:k-btn-shadow--success',
 	'k-button--success__focus':
 		'focus-visible:border-ikun-success focus-visible:k-btn-shadow--success',
+	'k-button--success__hover': 'hover:border-ikun-success',
 	'k-button--success__icon': 'text-ikun-success',
 
 	'k-button--error':
-		'bg-ikun-error-100 text-ikun-error border-ikun-error-100 hover:border-ikun-error',
+		'bg-ikun-error-100 text-ikun-error border-ikun-error-100',
 	'k-button--error__active': 'active:border-ikun-error active:k-btn-shadow--error',
 	'k-button--error__focus': 'focus-visible:border-ikun-error focus-visible:k-btn-shadow--error',
+	'k-button--error__hover': 'hover:border-ikun-error',
 	'k-button--error__icon': 'text-ikun-error',
 
 	'k-button--warning':
-		'bg-ikun-warning-100 text-ikun-warning border-ikun-warning-100 hover:border-ikun-warning',
+		'bg-ikun-warning-100 text-ikun-warning border-ikun-warning-100',
 	'k-button--warning__active': 'active:border-ikun-warning active:k-btn-shadow--warning',
 	'k-button--warning__focus':
 		'focus-visible:border-ikun-warning focus-visible:k-btn-shadow--warning',
+	'k-button--warning__hover': 'hover:border-ikun-warning',
 	'k-button--warning__icon': 'text-ikun-warning',
 
-	'k-button--info': 'bg-ikun-info-100 text-ikun-info border-ikun-info-100 hover:border-ikun-info',
+	'k-button--info': 'bg-ikun-info-100 text-ikun-info border-ikun-info-100',
 	'k-button--info__active': 'active:border-ikun-info active:k-btn-shadow--info',
 	'k-button--info__focus': 'focus-visible:border-ikun-info focus-visible:k-btn-shadow--info',
+	'k-button--info__hover': 'hover:border-ikun-info',
 	'k-button--info__icon': 'text-ikun-info'
 };
