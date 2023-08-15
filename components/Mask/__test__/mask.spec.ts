84ce13a37158a5e4e6688884345e49dc42d20295
白雾三语 <32354856+baiwusanyu-c@users.noreply.github.com>
2023-08-15 22:24:17 +0800

    test: added KMask unit test

---
+++ components/Mask/__test__/mask.spec.ts
@@ -0,0 +1,72 @@
+import { tick } from 'svelte';
+import { afterEach, expect, test, describe, beforeEach } from 'vitest';
+import KMask from '../src/index.svelte';
+
+let host: HTMLElement;
+
+const initHost = () => {
+	host = document.createElement('div');
+	host.setAttribute('id', 'host');
+	host.style.width = '100px';
+	host.style.height = '100px';
+	document.body.appendChild(host);
+};
+beforeEach(() => {
+	initHost();
+});
+afterEach(() => {
+	host.remove();
+});
+
+describe('Test: KMask', () => {
+	test('props: value', async () => {
+		const instance = new KMask({
+			target: host
+		});
+		expect(instance).toBeTruthy();
+		expect(host.innerHTML).not.toBeTruthy();
+
+		const instance2 = new KMask({
+			target: host,
+			props: {
+				value: true
+			}
+		});
+		expect(instance2).toBeTruthy();
+		expect(host.innerHTML.includes('top: 0px; left: 0px; width: 100%; height: 100%;')).toBeTruthy();
+		expect(host.innerHTML).matchSnapshot();
+	});
+
+	test('props: target', async () => {
+		const target = document.createElement('div');
+		target.setAttribute('id', 'target');
+		document.body.appendChild(host);
+		await tick();
+		const instance = new KMask({
+			target: host,
+			props: {
+				target,
+				value: true
+			}
+		});
+		await tick();
+		expect(instance).toBeTruthy();
+		expect(target.style.position).toBe('relative');
+		expect(target.style.overflow).matchSnapshot('hidden');
+		expect(host.innerHTML).matchSnapshot();
+	});
+
+	test('props: color', async () => {
+		const instance = new KMask({
+			target: host,
+			props: {
+				value: true,
+				color: 'red'
+			}
+		});
+		expect(instance).toBeTruthy();
+		expect(host.children[0]!.style.backgroundColor).toBe('red');
+		expect(host.innerHTML).matchSnapshot();
+	});
+});
+// TODO e2e test
