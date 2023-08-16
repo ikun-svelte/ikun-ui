fcfe01f44e2e32ff0f0803907844b6377e25f639
王野未 <826036140@qq.com>
2023-08-16 23:54:08 +0800

    feat(scrips): Add support for ‘/’ file paths

--- scripts/watch.js
+++ scripts/watch.js
@@ -1,19 +1,22 @@
-import { runTask } from './utils.js';
-import chokidar from 'chokidar';
+import chokidar from 'chokidar'
+import { runTask } from './utils.js'
+import { log, setGlobalPrefix } from 'baiwusanyu-utils'
+
+setGlobalPrefix('[ikun-ui]: ')
 
 function getComponentName(path) {
-	return path.split('\\')[1];
+	return path.split(/[\\/]/)[1]
 }
 
 chokidar
 	.watch('./components', {
 		ignored: /(dist)|(node_modules)/ // ignore dotfiles
 	})
-	.on('ready', () => console.log('Initial scan complete. Ready for changes'))
+	.on('ready', () => log('success', 'Initial scan complete. Ready for changes'))
 	.on('change', (path) => {
 		const component = getComponentName(path);
-		console.log(`${component} change,noew rebuilding.....`);
+		log('success', `${component} has been changed, rebuilding now.....`)
 		runTask(`npm run build`, `components/${component}`, 'watch', {
 			silent: false
-		});
-	});
+		})
+	})
