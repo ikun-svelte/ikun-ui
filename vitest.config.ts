67edfbf420148833e4f52ef1770bd4d7dc700b91
白雾三语 <32354856+baiwusanyu-c@users.noreply.github.com>
2023-08-17 10:46:46 +0800

    test: added KDrawer unit test (#96)

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite';
+import { defineConfig, Plugin } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 import sveltePreprocess from 'svelte-preprocess';
 export default defineConfig({
@@ -6,7 +6,17 @@ export default defineConfig({
 		svelte({
 			hot: !process.env.VITEST,
 			preprocess: [sveltePreprocess({ typescript: true })]
-		})
+		}),
+		{
+			name: 'compatible:svelte:kit',
+			post: 'pre',
+			resolveId(id: string) {
+				if (id === '$app/environment') return '\0$app/environment';
+			},
+			load(id: string) {
+				if (id === '\0$app/environment') return 'export const browser = true';
+			}
+		} as Plugin
 	],
 	test: {
 		globals: true,
