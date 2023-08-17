475767602e046be452e713a40d98b66da2965e1e
Victor Bo <hi@vtrbo.cn>
2023-08-17 12:29:52 +0800

    docs: add badge docs

---
+++ docs/example/badge/basic.svelte
@@ -0,0 +1,18 @@
+<script>
+	import { KButton } from '@ikun-ui/button';
+	import { KBadge } from '@ikun-ui/badge';
+
+	let count = 12;
+</script>
+
+<KBadge value={count} cls="mr-40px">
+	<KButton on:click={() => count++}>🐔太美</KButton>
+</KBadge>
+
+<KBadge value={666} type="warning" cls="mr-40px">
+	<KButton type="error">卤出鸡脚了吧</KButton>
+</KBadge>
+
+<KBadge value="666" type="primary" hidden cls="mr-40px">
+	<KButton type="warning">看不见我</KButton>
+</KBadge>
---
+++ docs/example/badge/max-value.svelte
@@ -0,0 +1,14 @@
+<script>
+	import { KButton } from '@ikun-ui/button';
+	import { KBadge } from '@ikun-ui/badge';
+
+	let count = 12;
+</script>
+
+<KBadge value={count} max={12} cls="mr-40px">
+	<KButton on:click={() => count++}>让我怎么荔枝</KButton>
+</KBadge>
+
+<KBadge value={666} max={25} type="warning" cls="mr-40px">
+	<KButton type="info">小黑子</KButton>
+</KBadge>
