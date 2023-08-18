79797b43c63db6f9d320065faee9e7b19d31121c
PanKeChun <2489964425@qq.com>
2023-08-18 16:23:36 +0800

    feat: create slider component

---
+++ components/Slider/src/index.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import { KTooltip } from '@ikun-ui/tooltip';
+	import type { IKunPlacement } from '@ikun-ui/utils';
+	export let value: number = 0;
+	export let min: number = 0;
+	export let max: number = 100;
+	export let disabled: boolean = false;
+	export let placement: IKunPlacement = 'top';
+
+	let content: string = '0';
+</script>
+
+<div class="k-slider--base">
+	<div class="k-slider--runway">
+		<div class="k-slider--bar"></div>
+		<div class="k-slider--button-wrapper">
+			<KTooltip {content} {placement}></KTooltip>
+		</div>
+	</div>
+</div>
