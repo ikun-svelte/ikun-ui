03800390ea869bb4587d3c3b3593c833c1fefd18
moushicheng <moushicheng@outlook.com>
2023-08-19 14:16:46 +0800

    feat: add type IkunUpdateFied

--- utils/src/types.ts
+++ utils/src/types.ts
@@ -4,3 +4,4 @@ export type IKunUncertainFunction<T = any> = () => T | void;
 export type IKunSize = 'sm' | 'md' | 'lg';
 export type IKunPlacement = 'top' | 'left' | 'right' | 'bottom';
 export type IKunTrigger = 'manual' | 'click' | 'hover';
+export type IKunUpdateField = (value?: any) => void;
