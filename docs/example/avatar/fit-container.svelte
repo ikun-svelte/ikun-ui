f96929ecb028e73c86c371d3eb1e7db52391f100
Victor Bo <hi@vtrbo.cn>
2023-08-21 18:28:54 +0800

    feat: add avatar component (#115)
    * feat: add avatar component
    
    * fix: remove extra space from class names
    
    * feat: avatar component add attrs props

---
+++ docs/example/avatar/fit-container.svelte
@@ -0,0 +1,15 @@
+<script>
+	import { KAvatar } from '@ikun-ui/avatar';
+
+	const src = 'https://www.svelte.cn/svelte-logo-horizontal.svg';
+	const fits = ['fill', 'contain', 'cover', 'none', 'scale-down'];
+</script>
+
+<div class="flex items-center justify-between">
+	{#each fits as fit}
+		<div>
+			<div class="text-center mb-6px text-12px color-#909399">{fit}</div>
+			<KAvatar {src} {fit} size={100} />
+		</div>
+	{/each}
+</div>
---
+++ docs/example/avatar/types.svelte
@@ -0,0 +1,9 @@
+<script>
+	import { KAvatar } from '@ikun-ui/avatar';
+</script>
+
+<div class="flex items-center justify-around">
+	<KAvatar src="https://ikun-ui.netlify.app/logo.svg"></KAvatar>
+	<KAvatar icon="i-carbon-settings"></KAvatar>
+	<KAvatar>User</KAvatar>
+</div>
