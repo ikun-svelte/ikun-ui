ed6093a1bce38a4fcfea0865ab0f6aa752debacb
baiwusanyu-c <740132583@qq.com>
2023-08-21 14:32:34 +0800

    chore: format code

--- components/Notification/__test__/notification.spec.ts
+++ components/Notification/__test__/notification.spec.ts
@@ -137,201 +137,201 @@ describe('Test: KNotify', () => {
 		expect(host.innerHTML.includes('top: calc(100% - 20px); left: 20px;')).toBeTruthy();
 	});
 
-		test('options: target', async () => {
-      const target = document.createElement('div');
-      target.setAttribute('id', 'target');
-      KNotify({
-        target
-      });
-      await tick();
-      await vi.advanceTimersByTimeAsync(150);
-      expect(target.innerHTML.includes('k-notification--base')).toBeTruthy();
-    });
+	test('options: target', async () => {
+		const target = document.createElement('div');
+		target.setAttribute('id', 'target');
+		KNotify({
+			target
+		});
+		await tick();
+		await vi.advanceTimersByTimeAsync(150);
+		expect(target.innerHTML.includes('k-notification--base')).toBeTruthy();
+	});
 
-    test('options: close', async () => {
-        KNotify({
-          target: host,
-          close: true
-        });
-        await tick();
-        await vi.advanceTimersByTimeAsync(150);
-        expect(host.innerHTML.includes('i-carbon-close')).toBeTruthy();
-      });
+	test('options: close', async () => {
+		KNotify({
+			target: host,
+			close: true
+		});
+		await tick();
+		await vi.advanceTimersByTimeAsync(150);
+		expect(host.innerHTML.includes('i-carbon-close')).toBeTruthy();
+	});
 
-    test('options: autoClose', async () => {
-        KNotify({
-          target: host,
-          close: true
-        });
-        await tick();
-        await vi.advanceTimersByTimeAsync(2000);
-        expect(host.innerHTML.includes('k-notification--base')).toBeTruthy();
-        await vi.advanceTimersByTimeAsync(4000);
-        expect(host.innerHTML.includes('k-notification--base')).not.toBeTruthy();
-      });
+	test('options: autoClose', async () => {
+		KNotify({
+			target: host,
+			close: true
+		});
+		await tick();
+		await vi.advanceTimersByTimeAsync(2000);
+		expect(host.innerHTML.includes('k-notification--base')).toBeTruthy();
+		await vi.advanceTimersByTimeAsync(4000);
+		expect(host.innerHTML.includes('k-notification--base')).not.toBeTruthy();
+	});
 
-    	test('options: onClose', async () => {
-        const mockFn = vi.fn();
-        KNotify({
-          target: host,
-          onClose: mockFn
-        });
-        await tick();
-        const btn = host.children[0].children[0].children[0].children[0];
-        btn.dispatchEvent(new window.Event('click', { bubbles: true }));
-        await tick();
-        expect(mockFn).toBeCalled();
-      });
+	test('options: onClose', async () => {
+		const mockFn = vi.fn();
+		KNotify({
+			target: host,
+			onClose: mockFn
+		});
+		await tick();
+		const btn = host.children[0].children[0].children[0].children[0];
+		btn.dispatchEvent(new window.Event('click', { bubbles: true }));
+		await tick();
+		expect(mockFn).toBeCalled();
+	});
 
-      test('options: duration', async () => {
-        KNotify({
-          target: host,
-          duration: 3500
-        });
-        await tick();
-        await vi.advanceTimersByTimeAsync(3500);
-        expect(host.innerHTML.includes('k-notification--base')).toBeTruthy();
-        await vi.advanceTimersByTimeAsync(1000);
-        expect(host.innerHTML.includes('k-notification--base')).not.toBeTruthy();
-      });
+	test('options: duration', async () => {
+		KNotify({
+			target: host,
+			duration: 3500
+		});
+		await tick();
+		await vi.advanceTimersByTimeAsync(3500);
+		expect(host.innerHTML.includes('k-notification--base')).toBeTruthy();
+		await vi.advanceTimersByTimeAsync(1000);
+		expect(host.innerHTML.includes('k-notification--base')).not.toBeTruthy();
+	});
 
-      test('options: offset', async () => {
-        KNotify({
-          target: host,
-          offset: 300
-        });
-        await tick();
-        await vi.advanceTimersByTimeAsync(300);
-        expect(host.innerHTML.includes('top: 320px; left: calc(100% - 20px);')).toBeTruthy();
-      });
+	test('options: offset', async () => {
+		KNotify({
+			target: host,
+			offset: 300
+		});
+		await tick();
+		await vi.advanceTimersByTimeAsync(300);
+		expect(host.innerHTML.includes('top: 320px; left: calc(100% - 20px);')).toBeTruthy();
+	});
 
-      test('options: title is string', async () => {
-        KNotify({
-          target: host,
-          title: 'foo'
-        });
-        await tick();
-        await vi.advanceTimersByTimeAsync(300);
-        expect(host.innerHTML.includes('foo')).toBeTruthy();
-      });
+	test('options: title is string', async () => {
+		KNotify({
+			target: host,
+			title: 'foo'
+		});
+		await tick();
+		await vi.advanceTimersByTimeAsync(300);
+		expect(host.innerHTML.includes('foo')).toBeTruthy();
+	});
 
-      test('options: title is svelte component', async () => {
-        KNotify({
-          target: host,
-          title: KNotifyTitle
-        });
-        await tick();
-        await vi.advanceTimersByTimeAsync(300);
-        expect(host.innerHTML.includes('notification_title')).toBeTruthy();
-      });
+	test('options: title is svelte component', async () => {
+		KNotify({
+			target: host,
+			title: KNotifyTitle
+		});
+		await tick();
+		await vi.advanceTimersByTimeAsync(300);
+		expect(host.innerHTML.includes('notification_title')).toBeTruthy();
+	});
 
-      test('options: content is string', async () => {
-        KNotify({
-          target: host,
-          content: 'bar'
-        });
-        await tick();
-        await vi.advanceTimersByTimeAsync(300);
-        expect(host.innerHTML.includes('bar')).toBeTruthy();
-      });
+	test('options: content is string', async () => {
+		KNotify({
+			target: host,
+			content: 'bar'
+		});
+		await tick();
+		await vi.advanceTimersByTimeAsync(300);
+		expect(host.innerHTML.includes('bar')).toBeTruthy();
+	});
 
-      test('options: content is svelte component', async () => {
-        KNotify({
-          target: host,
-          title: KNotifyContent
-        });
-        await tick();
-        await vi.advanceTimersByTimeAsync(300);
-        expect(host.innerHTML.includes('notification_content')).toBeTruthy();
-      });
+	test('options: content is svelte component', async () => {
+		KNotify({
+			target: host,
+			title: KNotifyContent
+		});
+		await tick();
+		await vi.advanceTimersByTimeAsync(300);
+		expect(host.innerHTML.includes('notification_content')).toBeTruthy();
+	});
 
-      test('api: clear', async () => {
-        const inst = KNotify({
-          target: host,
-          title: KNotifyContent
-        });
-        await tick();
-        await vi.advanceTimersByTimeAsync(300);
-        expect(host.innerHTML.includes('k-notification--base')).toBeTruthy();
-        KNotify.clear(inst as NotifyComponent);
-        await tick();
-        await vi.advanceTimersByTimeAsync(300);
-        expect(host.innerHTML.includes('k-notification--base')).not.toBeTruthy();
-      });
+	test('api: clear', async () => {
+		const inst = KNotify({
+			target: host,
+			title: KNotifyContent
+		});
+		await tick();
+		await vi.advanceTimersByTimeAsync(300);
+		expect(host.innerHTML.includes('k-notification--base')).toBeTruthy();
+		KNotify.clear(inst as NotifyComponent);
+		await tick();
+		await vi.advanceTimersByTimeAsync(300);
+		expect(host.innerHTML.includes('k-notification--base')).not.toBeTruthy();
+	});
 
-      test('api: clearAll', async () => {
-        KNotify({
-          target: host,
-          title: 'KNotifyContent1'
-        });
-        KNotify({
-          target: host,
-          title: 'KNotifyContent2'
-        });
-        await tick();
-        await vi.advanceTimersByTimeAsync(300);
-        expect(host.innerHTML.includes('KNotifyContent1')).toBeTruthy();
-        expect(host.innerHTML.includes('KNotifyContent2')).toBeTruthy();
-        KNotify.clearAll();
-        await tick();
-        await vi.advanceTimersByTimeAsync(300);
-        expect(host.innerHTML.includes('KNotifyContent1')).not.toBeTruthy();
-        expect(host.innerHTML.includes('KNotifyContent2')).not.toBeTruthy();
-      });
+	test('api: clearAll', async () => {
+		KNotify({
+			target: host,
+			title: 'KNotifyContent1'
+		});
+		KNotify({
+			target: host,
+			title: 'KNotifyContent2'
+		});
+		await tick();
+		await vi.advanceTimersByTimeAsync(300);
+		expect(host.innerHTML.includes('KNotifyContent1')).toBeTruthy();
+		expect(host.innerHTML.includes('KNotifyContent2')).toBeTruthy();
+		KNotify.clearAll();
+		await tick();
+		await vi.advanceTimersByTimeAsync(300);
+		expect(host.innerHTML.includes('KNotifyContent1')).not.toBeTruthy();
+		expect(host.innerHTML.includes('KNotifyContent2')).not.toBeTruthy();
+	});
 
-      test('api: update', async () => {
-        const inst = KNotify({
-          target: host,
-          title: 'KNotifyContent test update'
-        });
-        await tick();
-        await vi.advanceTimersByTimeAsync(300);
-        expect(host.innerHTML.includes('KNotifyContent test update')).toBeTruthy();
-        KNotify.update(inst as NotifyComponent, {
-          title: 'KNotifyContent test change'
-        });
-        await tick();
-        await vi.advanceTimersByTimeAsync(300);
-        expect(host.innerHTML.includes('KNotifyContent test change')).toBeTruthy();
-      });
+	test('api: update', async () => {
+		const inst = KNotify({
+			target: host,
+			title: 'KNotifyContent test update'
+		});
+		await tick();
+		await vi.advanceTimersByTimeAsync(300);
+		expect(host.innerHTML.includes('KNotifyContent test update')).toBeTruthy();
+		KNotify.update(inst as NotifyComponent, {
+			title: 'KNotifyContent test change'
+		});
+		await tick();
+		await vi.advanceTimersByTimeAsync(300);
+		expect(host.innerHTML.includes('KNotifyContent test change')).toBeTruthy();
+	});
 
-      test('api: info', async () => {
-        KNotify.info({
-          target: host,
-          title: 'KNotifyContent'
-        });
-        await tick();
-        expect(host.innerHTML.includes('k-notification--base')).toBeTruthy();
-        expect(host.innerHTML.includes('k-notification--icon--info')).toBeTruthy();
-      });
+	test('api: info', async () => {
+		KNotify.info({
+			target: host,
+			title: 'KNotifyContent'
+		});
+		await tick();
+		expect(host.innerHTML.includes('k-notification--base')).toBeTruthy();
+		expect(host.innerHTML.includes('k-notification--icon--info')).toBeTruthy();
+	});
 
-      test('api: warning', async () => {
-        KNotify.warning({
-          target: host,
-          title: 'KNotifyContent'
-        });
-        await tick();
-        expect(host.innerHTML.includes('k-notification--base')).toBeTruthy();
-        expect(host.innerHTML.includes('k-notification--icon--warning')).toBeTruthy();
-      });
+	test('api: warning', async () => {
+		KNotify.warning({
+			target: host,
+			title: 'KNotifyContent'
+		});
+		await tick();
+		expect(host.innerHTML.includes('k-notification--base')).toBeTruthy();
+		expect(host.innerHTML.includes('k-notification--icon--warning')).toBeTruthy();
+	});
 
-      test('api: success', async () => {
-        KNotify.success({
-          target: host,
-          title: 'KNotifyContent'
-        });
-        await tick();
-        expect(host.innerHTML.includes('k-notification--base')).toBeTruthy();
-        expect(host.innerHTML.includes('k-notification--icon--success')).toBeTruthy();
-      });
+	test('api: success', async () => {
+		KNotify.success({
+			target: host,
+			title: 'KNotifyContent'
+		});
+		await tick();
+		expect(host.innerHTML.includes('k-notification--base')).toBeTruthy();
+		expect(host.innerHTML.includes('k-notification--icon--success')).toBeTruthy();
+	});
 
-      test('api: error', async () => {
-        KNotify.error({
-          target: host,
-          title: 'KNotifyContent'
-        });
-        await tick();
-        expect(host.innerHTML.includes('k-notification--base')).toBeTruthy();
-        expect(host.innerHTML.includes('k-notification--icon--error')).toBeTruthy();
-      });
+	test('api: error', async () => {
+		KNotify.error({
+			target: host,
+			title: 'KNotifyContent'
+		});
+		await tick();
+		expect(host.innerHTML.includes('k-notification--base')).toBeTruthy();
+		expect(host.innerHTML.includes('k-notification--icon--error')).toBeTruthy();
+	});
 });
