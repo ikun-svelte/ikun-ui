3b727787a1c7372b8e356bcb94419617b6970a9f
Victor Bo <hi@vtrbo.cn>
2023-08-21 16:37:59 +0800

    feat: add avatar component

---
+++ components/Avatar/src/index.svelte
@@ -0,0 +1,49 @@
+<script lang="ts">
+	import { createEventDispatcher } from 'svelte';
+	import { KIcon } from '@ikun-ui/icon';
+	import { isNumber } from 'baiwusanyu-utils';
+
+	export let icon: string = '';
+	export let src: string = '';
+	export let srcSet: string = '';
+	export let alt: string = '';
+	export let fit: 'fill' | 'contain' | 'cover' | 'none' | 'scale-down' = 'cover';
+	export let size: string | number = '50px';
+	export let radius: string | number = '4px';
+	export let cls: string = '';
+
+	let avatarSize: string = size;
+	$: if (isNumber(size)) {
+		avatarSize = `${size}px`;
+	}
+
+	let avatarRadius: string = radius;
+	$: if (isNumber(radius)) {
+		avatarRadius = `${radius}px`;
+	}
+
+	let hasLoadError: boolean = false;
+	$: if (src) {
+		hasLoadError = false;
+	}
+
+	const dispatch = createEventDispatcher();
+
+	const onError = (event: CustomEvent) => {
+		hasLoadError = true;
+		dispatch('error', event);
+	};
+</script>
+
+<div
+	class="k-avatar {cls}"
+	style="width: {avatarSize}; height: {avatarSize}; border-radius: {avatarRadius}"
+>
+	{#if (src || srcSet) && !hasLoadError}
+		<img {src} {alt} {srcSet} on:error={onError} style="object-fit: {fit}" />
+	{:else if icon}
+		<KIcon v-else-if="icon" {icon}></KIcon>
+	{:else}
+		<slot v-else />
+	{/if}
+</div>
---
+++ docs/example/avatar/basic.svelte
@@ -0,0 +1,12 @@
+<script>
+	import { KAvatar } from '@ikun-ui/avatar';
+
+	const src = 'https://ikun-ui.netlify.app/logo.svg';
+</script>
+
+<div class="flex items-center justify-around">
+	<KAvatar {src}></KAvatar>
+	<KAvatar {src} radius={0}></KAvatar>
+	<KAvatar {src} size={60} radius={10}></KAvatar>
+	<KAvatar {src} size="5rem" radius="50%"></KAvatar>
+</div>
