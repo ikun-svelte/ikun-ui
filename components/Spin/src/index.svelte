2c57d0c021c0e8ef9911c9cacaaac1f8568a87ef
白雾三语 <32354856+baiwusanyu-c@users.noreply.github.com>
2023-08-22 23:35:35 +0800

    update: remove target props with drawer (#132)
    * update: remove target props with drawer
    
    * update: remove target props with drawer

--- components/Spin/src/index.svelte
+++ components/Spin/src/index.svelte
@@ -2,6 +2,7 @@
 	import { KIcon } from '@ikun-ui/icon';
 	import { KMask } from '@ikun-ui/mask';
 	import { isFunction, isString } from 'baiwusanyu-utils';
+	import {KClientOnly} from "@ikun-ui/client-only";
 	export let show: boolean = false;
 	export let text = '';
 	export let spinner: any = 'i-carbon-circle-dash';
@@ -12,23 +13,24 @@
 	export let target = document.body;
 	export let rotating: boolean = true;
 </script>
-
-<KMask value={show}
-	   color={background}
-	   {target} >
-	<div class="{cls}" {...attrs}>
-		<div class="k-spin--spinner">
-			<div class={rotating ? 'k-spin--spinner__rotating' : ''}>
-				{#if spinner && isString(spinner)}
-					<KIcon icon={spinner} color="text-ikun-main" />
-				{:else if spinner && isFunction(spinner)}
-					<svelte:component this={spinner} />
+<KClientOnly>
+	<KMask value={show}
+		   color={background}
+		   {target} >
+		<div class="{cls}" {...attrs}>
+			<div class="k-spin--spinner">
+				<div class={rotating ? 'k-spin--spinner__rotating' : ''}>
+					{#if spinner && isString(spinner)}
+						<KIcon icon={spinner} color="text-ikun-main" />
+					{:else if spinner && isFunction(spinner)}
+						<svelte:component this={spinner} />
+					{/if}
+				</div>
+				{#if text}
+					<p class="k-spin--text">{text}</p>
 				{/if}
 			</div>
-			{#if text}
-				<p class="k-spin--text">{text}</p>
-			{/if}
 		</div>
-	</div>
-</KMask>
+	</KMask>
+</KClientOnly>
 
