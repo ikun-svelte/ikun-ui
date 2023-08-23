d2865a23258becb92b865b6846b6a783e0f6df26
PanKeChun <2489964425@qq.com>
2023-08-23 09:45:35 +0800

    docs: add slider docs

---
+++ docs/example/slider/range.svelte
@@ -0,0 +1,16 @@
+<script>
+    import { KSlider } from "@ikun-ui/slider";
+
+    let min = 10;
+    let max = 20;
+    let value = 15;
+    const handleInput = ( event ) => {
+        value = event.detail;
+    };
+
+</script>
+
+<KSlider min={min} max={max} on:input={handleInput} value={value}></KSlider>
+<p class="px-2">value: {value}</p>
+<p class="px-2">min: {min}</p>
+<p class="px-2">max: {max}</p>
