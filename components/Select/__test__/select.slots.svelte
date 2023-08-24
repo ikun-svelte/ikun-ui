d01ffddb2cb6341a43c0057955cf0e7e2583590a
PanKeChun <2489964425@qq.com>
2023-08-24 14:30:46 +0800

    test: added select unit test

---
+++ components/Select/__test__/select.slots.svelte
@@ -0,0 +1,11 @@
+<script>
+    import KSelect from '../src';
+</script>
+
+<KSelect value={'Normal'}>
+    <div class="prefix" slot="prefix">prefix</div>
+    <div class="suffix" slot="suffix">suffix</div>
+    {#each ['Tiny', 'Small', 'Normal', 'Large', 'Huge'] as item (item)}
+        <option>{item}</option>
+    {/each}
+</KSelect>
