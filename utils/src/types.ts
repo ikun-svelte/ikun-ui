669bb2bf82a2cf02e820ee4d89a56c342b42a606
moushicheng <moushicheng@outlook.com>
2023-08-27 11:35:50 +0800

    chore: additional comment in IkunFormInstance

--- utils/src/types.ts
+++ utils/src/types.ts
@@ -12,17 +12,29 @@ export type Contexts = {
 	__this__: FormContext;
 };
 export type IKunFormInstance = {
+	//record Field value
 	values: any;
+	//record Field Contexts
 	contexts: Contexts;
+	//get Field Values
 	submit: () => void;
+	//validate all Field value
 	validateValues: () => boolean;
+	//get specific Field value
 	getValue: (path: string) => any;
+	//set specific Field value
 	setValue: (path: string, value: any) => void;
+	//set all Field value
 	setValues: (values: any) => void;
+	//record specific contexts into IKunFormInstance.contexts
 	setContext: (path: string, context: FormContext) => void;
+	//reset Field Values using field contexts inititalValue
 	resetValue: (path: string) => void;
+	//reset all Field Values
 	resetValues: () => void;
+	//subscribe specific Field change ,callback will be called when change
 	subscribe: (path: string, callback: (values: any) => void) => void;
+	//subscribe all Field change,callback will be called when change
 	subscribes: (callback: (values: any) => void) => void;
 };
 
