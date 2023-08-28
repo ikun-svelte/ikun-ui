c0d6cc530590f55d4c2a83a794f7b32c81ca7cef
pkc918 <2489964425@qq.com>
2023-08-28 10:35:13 +0800

    test: added popover unit test

---
+++ components/Popover/__test__/popover.spec.ts
@@ -0,0 +1,77 @@
+import { tick } from 'svelte';
+import { afterEach, beforeEach, describe, expect, test, vi } from 'vitest';
+import KPopoverSlot from './popover.placement.svelte';
+import KPopoverTrigger from './popover.trigger.svelte';
+import KPopoverSlots from './popover.slots.svelte';
+
+let host: HTMLElement;
+
+const initHost = () => {
+	host = document.createElement('div');
+	host.setAttribute('id', 'host');
+	document.body.appendChild(host);
+};
+beforeEach(() => {
+	initHost();
+	vi.useFakeTimers();
+});
+afterEach(() => {
+	host.remove();
+	vi.useRealTimers();
+});
+
+describe('Test: KPopover', () => {
+	test('props: placement', async () => {
+		const instance = new KPopoverSlot({
+			target: host
+		});
+		expect(instance).toBeTruthy();
+		await tick();
+		const Elm = host.children[0].getElementsByTagName('div')[0];
+		Elm.dispatchEvent(
+			new MouseEvent('mouseenter', {
+				cancelable: true
+			})
+		);
+		vi.runAllTimers();
+		await tick();
+		expect(host.innerHTML.includes('有美一人，清扬婉兮')).toBeTruthy();
+		// TODO
+		// expect(host.innerHTML.includes('data-popper-arrow-top')).toBeTruthy();
+		expect(host.innerHTML).matchSnapshot();
+	});
+
+	test('props: trigger', async () => {
+		const instance = new KPopoverTrigger({
+			target: host
+		});
+		expect(instance).toBeTruthy();
+		await tick();
+		const Elm = host.children[0].getElementsByTagName('div')[0];
+		Elm.click();
+		vi.runAllTimers();
+		await tick();
+		expect(host.innerHTML.includes('有美一人，清扬婉兮')).toBeTruthy();
+		expect(host.innerHTML).matchSnapshot();
+	});
+
+	test('slots: triggerEl & contentEl', async () => {
+		const instance = new KPopoverSlots({
+			target: host
+		});
+		expect(instance).toBeTruthy();
+		await tick();
+		const Elm = host.children[0].getElementsByTagName('div')[0];
+		Elm.dispatchEvent(
+			new MouseEvent('mouseenter', {
+				cancelable: true
+			})
+		);
+		vi.runAllTimers();
+		await tick();
+		expect(
+			host.innerHTML.includes('triggerEl') && host.innerHTML.includes('contentEl')
+		).toBeTruthy();
+		expect(host.innerHTML).matchSnapshot();
+	});
+});
