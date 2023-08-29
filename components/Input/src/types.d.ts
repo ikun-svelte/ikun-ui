fa590a13eb04eefe20f868d99aa7b2f22fd1160b
Victor Bo <hi@vtrbo.cn>
2023-08-29 08:53:21 +0800

    fix: input bind value can't be updated in real time (#152)
    * fix: input bind value can't update, rm extra space
    
    * fix: modify style and added unit test

--- components/Input/src/types.d.ts
+++ components/Input/src/types.d.ts
@@ -1,1 +1,12 @@
 /// <reference types="svelte" />
+export type KInputProps = {
+	value: string;
+	placeholder: string;
+	disabled: boolean;
+	iconPrefix: string;
+	iconSuffix: string;
+	isError: boolean;
+	errorMsg: string;
+	cls: string;
+	attrs: Record<string, string>;
+};
