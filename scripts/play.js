dc8de37b04807c7f48228bdfb02f8df9aad7eca1
baiwusanyu-c <740132583@qq.com>
2023-08-31 17:02:32 +0800

    fix: Fix flicker when mask is off

--- scripts/play.js
+++ scripts/play.js
@@ -1,8 +1,8 @@
 /**
  * Create play template
  */
-import * as fs from 'fs'
-const PLAY_PATH = 'play/src/routes'
+import * as fs from 'fs';
+const PLAY_PATH = 'play/src/routes';
 const content = `<script lang="ts">
 import 'virtual:uno.css';
 import { KButton } from '@ikun-ui/button'
@@ -10,17 +10,17 @@ import { KButton } from '@ikun-ui/button'
 <KButton type="info" cls="mx-2">
 info
 </KButton>
-`
-function createPlayTemplate(){
-  if (!fs.existsSync(`${PLAY_PATH}/+page.svelte`)) {
-    fs.mkdirSync(PLAY_PATH, { recursive: true });
-    fs.writeFile(`${PLAY_PATH}/+page.svelte`, content, (err) => {
-      if (err) {
-        console.error('Error writing file:', err);
-      } else {
-        console.log(`File +page.svelte has been created with the specified content.`);
-      }
-    });
-  }
+`;
+function createPlayTemplate() {
+	if (!fs.existsSync(`${PLAY_PATH}/+page.svelte`)) {
+		fs.mkdirSync(PLAY_PATH, { recursive: true });
+		fs.writeFile(`${PLAY_PATH}/+page.svelte`, content, (err) => {
+			if (err) {
+				console.error('Error writing file:', err);
+			} else {
+				console.log(`File +page.svelte has been created with the specified content.`);
+			}
+		});
+	}
 }
-createPlayTemplate()
+createPlayTemplate();
