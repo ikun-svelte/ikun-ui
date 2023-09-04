bd3e93fc83b4b180eab82ab4aa1c094a3c6d962b
baiwusanyu-c <740132583@qq.com>
2023-09-04 13:31:37 +0800

    style: Optimize popover component shadow

--- preset/src/shortcuts/popover.ts
+++ preset/src/shortcuts/popover.ts
@@ -1,7 +1,15 @@
 export const popoverShortcuts: Record<string, string> = {
 	// popover
-	'k-popover--base': 'rounded p2 bg-ikun-white shadow-lg z-999 box-border text-ikun-tx-base',
+	'k-popover--base': 'rounded p2 bg-ikun-white  z-999 box-border text-ikun-tx-base',
+	'k-popover--base__left': 'k-popover-shadow__left',
+	'k-popover--base__top': 'k-popover-shadow__top',
+	'k-popover--base__right': 'k-popover-shadow__right',
+	'k-popover--base__bottom': 'k-popover-shadow__bottom',
 	// dark
 	'k-popover--base__dark':
-		'dark:bg-ikun-dark-300 dark:shadow-ikun-main dark:shadow dark:text-ikun-white'
+		'dark:bg-ikun-dark-300 dark:text-ikun-white',
+	'k-popover--base__left__dark': 'dark:k-popover-shadow__left__dark',
+	'k-popover--base__top__dark': 'dark:k-popover-shadow__top__dark',
+	'k-popover--base__right__dark': 'dark:k-popover-shadow__right__dark',
+	'k-popover--base__bottom__dark': 'dark:k-popover-shadow__bottom__dark',
 };
