41f12cd8d57629c4af568ac1de725b1868cf503b
baiwusanyu-c <740132583@qq.com>
2023-09-06 12:42:30 +0800

    test: updated test snap

--- components/Checkbox/src/index.svelte
+++ components/Checkbox/src/index.svelte
@@ -3,16 +3,16 @@
 	import { fade } from 'svelte/transition';
 	import { KIcon } from '@ikun-ui/icon';
 	import { clsx, type ClassValue } from 'clsx';
+	import { getPrefixCls } from '@ikun-ui/utils';
 
 	export let disabled = false;
 	export let value = false;
-	export let cls: ClassValue = '';
-	export let attrs = {};
+	export let cls: ClassValue = undefined;
+	export let attrs: Record<string, string> = {};
 	export let label = '';
 	// updateValue
 	const dispatch = createEventDispatcher();
 
-	$: cnames = clsx(cls);
 	$: valueInner = value;
 	let classChecking = '';
 	const handleUpdateValue = () => {
@@ -24,18 +24,39 @@
 			classChecking = '';
 		}, 300);
 	};
+
+	// class
+	const prefixCls = getPrefixCls('checkbox');
+	$: cnames = clsx(
+		`${prefixCls}--base`,
+		`${prefixCls}--base__dark`,
+		{
+			[`k-cur-disabled`]: disabled
+		},
+		cls
+	);
+
+	$: boxCls = clsx(
+		`${prefixCls}--box`,
+		{
+			[`bg-ikun-main border-ikun-main`]: valueInner && !disabled,
+			[`${prefixCls}--box__disabled`]: disabled
+		},
+		classChecking
+	);
+
+	$: labelCls = clsx(
+		`${prefixCls}--label`,
+		{
+			[`text-ikun-main`]: valueInner && !disabled
+		},
+		classChecking
+	);
 </script>
 
-<label
-	class="k-checkbox--base k-checkbox--base__dark {cnames} {disabled ? 'k-cur-disabled' : ''}"
-	{...attrs}
->
+<label class={cnames} {...attrs}>
 	<input value={valueInner} {disabled} type="checkbox" on:change={handleUpdateValue} hidden />
-	<div
-		class="k-checkbox--box
-				{valueInner && !disabled ? 'bg-ikun-main border-ikun-main' : ''}
-				{classChecking} {disabled ? 'k-checkbox--box__disabled' : ''}"
-	>
+	<div class={boxCls}>
 		{#if valueInner}
 			<div out:fade={{ duration: 200 }} in:fade={{ duration: 200 }}>
 				<KIcon icon="i-carbon-checkmark" color="!text-white" width="16px" height="16px" />
@@ -44,7 +65,7 @@
 	</div>
 	<slot>
 		{#if label}
-			<span class="k-checkbox--label {valueInner && !disabled ? 'text-ikun-main' : ''}">
+			<span class={labelCls}>
 				{label}
 			</span>
 		{/if}
