001a6fa2b9b5476f220510d5bf6fc3f833b42b89
pkc918 <xiaopan2000918@gmail.com>
2023-09-13 01:57:07 +0800

    fix: Fixed the problem that the spin component rotation style error

--- preset/src/shortcuts/spin.ts
+++ preset/src/shortcuts/spin.ts
@@ -2,5 +2,5 @@ export const spinShortcuts: Record<string, string> = {
 	'k-spin--spinner':
 		'absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-center text-ikun-main',
 	'k-spin--text': 'text-4 my-4 animate-bounce',
-	'k-spin--spinner__rotating': 'animate-spin animate-duration-3000'
+	'k-spin--spinner__rotating': 'animate-spin animate-duration-3000 fcc'
 };
