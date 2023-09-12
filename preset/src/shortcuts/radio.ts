c5a47da4139cdf9ceadea4149e8527d1f5fdddfd
pkc918 <xiaopan2000918@gmail.com>
2023-09-13 02:45:44 +0800

    fix: Fixed the problem that the radio component style is missing

--- preset/src/shortcuts/radio.ts
+++ preset/src/shortcuts/radio.ts
@@ -4,6 +4,7 @@ export const radioShortcuts: Record<string, string> = {
 	'k-radio--box': 'rounded-50% k-border border-ikun-bd-base f-c h-4 w-4',
 	'k-radio--box__disabled': 'k-bg-disabled',
 	'k-radio--label': 'text-base',
+	'k-radio--selected': 'bg-ikun-main border-ikun-main',
 	// dark
 	'k-radio--base__dark': 'dark:text-ikun-white'
 };
