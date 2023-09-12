ddf18452c7e215c49e2d68733b3a01fb6e6a40e7
baiwusanyu-c <740132583@qq.com>
2023-09-12 17:04:22 +0800

    chore: temp commit

---
+++ components/CheckboxGroup/__test__/group.base.svelte
@@ -0,0 +1,31 @@
+<script>
+	import 'virtual:uno.css';
+	import { KCheckbox } from '@ikun-ui/checkbox';
+	import { KCheckboxGroup } from '@ikun-ui/checkbox-group';
+
+	let value = ['3'];
+	let disabled = false;
+	const handleUpdated = (e) => {
+		value = e.detail;
+	};
+	const handleChange = () => {
+		value = ['1'];
+	};
+</script>
+
+<div class="h-full" style="width: 600px">
+	{value}
+	<div
+		on:click={() => {
+			disabled = !disabled;
+		}}
+	>
+		disabled
+	</div>
+	<div on:click={handleChange}>change</div>
+	<KCheckboxGroup {value} on:updateValue={handleUpdated} {disabled}>
+		<KCheckbox uid="1">1</KCheckbox>
+		<KCheckbox uid="2">2</KCheckbox>
+		<KCheckbox uid="3">3</KCheckbox>
+	</KCheckboxGroup>
+</div>
