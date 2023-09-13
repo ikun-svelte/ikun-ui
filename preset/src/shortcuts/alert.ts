4290fe5e6f7846334b0c5d627563858a44544be1
Victor Bo <hi@vtrbo.cn>
2023-09-13 11:07:37 +0800

    fix: The color of the close button of the alert component is wrong (#244)

--- preset/src/shortcuts/alert.ts
+++ preset/src/shortcuts/alert.ts
@@ -8,7 +8,7 @@ export const alertShortcuts = {
 	'k-alert--success__icon': 'i-carbon-checkmark-filled',
 	'k-alert--success__icon-color': 'text-ikun-success',
 	'k-alert--success__close-icon':
-		'hover:(bg-ikun-success [&>div.k-icon--base]:text-ikun-white b-rd-50%) active:(b-ikun-success k-btn-shadow--success)',
+		'hover:(bg-ikun-success text-ikun-white b-rd-50%) active:(b-ikun-success k-btn-shadow--success)',
 
 	// error
 	'k-alert--error': 'bg-ikun-error-100 text-ikun-error',
@@ -16,7 +16,7 @@ export const alertShortcuts = {
 	'k-alert--error__icon': 'i-carbon-error-filled',
 	'k-alert--error__icon-color': 'text-ikun-error',
 	'k-alert--error__close-icon':
-		'hover:(bg-ikun-error [&>div.k-icon--base]:text-ikun-white b-rd-50%) active:(b-ikun-error k-btn-shadow--error)',
+		'hover:(bg-ikun-error text-ikun-white b-rd-50%) active:(b-ikun-error k-btn-shadow--error)',
 
 	// warning
 	'k-alert--warning': 'bg-ikun-warning-100 text-ikun-warning',
@@ -24,7 +24,7 @@ export const alertShortcuts = {
 	'k-alert--warning__icon': 'i-carbon-warning-filled',
 	'k-alert--warning__icon-color': 'text-ikun-warning',
 	'k-alert--warning__close-icon':
-		'hover:(bg-ikun-warning [&>div.k-icon--base]:text-ikun-white b-rd-50%) active:(b-ikun-warning k-btn-shadow--warning)',
+		'hover:(bg-ikun-warning text-ikun-white b-rd-50%) active:(b-ikun-warning k-btn-shadow--warning)',
 
 	// info
 	'k-alert--info': 'bg-ikun-info-100 text-ikun-info',
@@ -32,7 +32,7 @@ export const alertShortcuts = {
 	'k-alert--info__icon': 'i-carbon-information-filled',
 	'k-alert--info__icon-color': 'text-ikun-info',
 	'k-alert--info__close-icon':
-		'hover:(bg-ikun-info-500 [&>div.k-icon--base]:text-ikun-white b-rd-50%) active:(b-ikun-info-500 k-btn-shadow--info)',
+		'hover:(bg-ikun-info-500 text-ikun-white b-rd-50%) active:(b-ikun-info-500 k-btn-shadow--info)',
 
 	'k-alert--content': 'text-14px flex-1',
 	'k-alert--title': 'font-600',
@@ -40,6 +40,6 @@ export const alertShortcuts = {
 	'k-alert--description__no-title': 'mt-0',
 
 	'k-alert--close': 'text-12px inline-flex cursor-pointer ml-8px',
-	'k-alert--close__icon-color': '[&,&>div.k-icon--base]:color-ikun-stone-400',
+	'k-alert--close__icon-color': 'color-ikun-stone-400',
 	'k-alert--close__has-description': 'absolute top-10px right-16px'
 };
