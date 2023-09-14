c75be99cc8c5a69a3c47b881fe90a85a2df970f4
baiwusanyu-c <740132583@qq.com>
2023-09-14 15:32:41 +0800

    docs: added layout component document

--- preset/src/shortcuts/layout.ts
+++ preset/src/shortcuts/layout.ts
@@ -1,13 +1,13 @@
 export const layoutShortcuts: Record<string, string> = {
 	// main
-	'k-main--base': 'box-border block basis-auto grow-1 shrink-1 basis-0% p-4',
+	'k-main--base': 'box-border block grow-1 shrink-1 basis-auto p-4',
 	// container
-	'k-container--base': 'box-border flex basis-auto flex-row grow-1 shrink-1 min-w-0',
+	'k-container--base': 'box-border flex flex-row grow-1 shrink-1 min-w-0 basis-auto',
 	'k-container--base__vertical': ' flex-col',
 	// header
-	'k-header--base': 'shrink-0 box-border min-h-60px px-4',
+	'k-header--base': 'shrink-0 box-border px-4',
 	// footer
-	'k-footer--base': 'shrink-0 box-border min-h-60px px-4',
+	'k-footer--base': 'shrink-0 box-border px-4',
 	// aside
-	'k-aside--base': 'overflow-auto shrink-0 box-border min-h-300px'
+	'k-aside--base': 'overflow-auto shrink-0 box-border'
 };
