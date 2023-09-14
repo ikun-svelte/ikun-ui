67d63aa9b34a1cf7e1a62276f4b4bc2ff9c3b9ac
Victor Bo <hi@vtrbo.cn>
2023-09-14 16:46:44 +0800

    style: modify rate disabled shortcuts

--- preset/src/shortcuts/rate.ts
+++ preset/src/shortcuts/rate.ts
@@ -3,7 +3,7 @@ export const rateShortcuts: Record<string, string> = {
 	'k-rate': 'fsc',
 
 	// disabled
-	'k-rate--disabled': 'op60 cursor-not-allowed',
+	'k-rate--disabled': 'op60 k-cur-disabled',
 
 	// readonly
 	'k-rate--readonly': 'cursor-auto',
@@ -11,7 +11,7 @@ export const rateShortcuts: Record<string, string> = {
 	// item
 	'k-rate--item': 'infcc',
 	'k-rate--item__not-readonly': 'cursor-pointer',
-	'k-rate--item__disabled': 'cursor-not-allowed',
+	'k-rate--item__disabled': 'k-cur-disabled',
 
 	// text
 	'k-rate--text': 'ml-6px text-10px line-height-none'
