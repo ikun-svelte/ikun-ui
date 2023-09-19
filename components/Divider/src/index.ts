fdb050ccb38325a42dad893a1058468e8edd3971
Victor Bo <hi@vtrbo.cn>
2023-09-19 19:26:10 +0800

    feat: added divider component (#265)
    * feat: added divider component
    
    * docs: added divider doc
    
    * docs: added divider component unit test
    
    * feat: modify divider export, doc and style

---
+++ components/Divider/src/index.ts
@@ -0,0 +1,5 @@
+/// <reference types="./types" />
+import Divider from './index.svelte';
+export { Divider as KDivider };
+
+export default Divider;
---
+++ docs/example/divider/line-style.svelte
@@ -0,0 +1,15 @@
+<script>
+	import { KDivider } from '@ikun-ui/divider';
+</script>
+
+<div class="px-10px">
+	<span>I never consider ease and joyfulness the purpose of life itself.</span>
+	<KDivider borderStyle="dotted" borderColor="b-red"></KDivider>
+	<span>I am not afraid of tomorrow for I have seen yesterday and love today.</span>
+	<KDivider borderStyle="dashed" borderColor="b-blue"></KDivider>
+	<span>Courage is the ladder on which all the other virtues mount.</span>
+	<KDivider borderStyle="solid" borderColor="b-green"></KDivider>
+	<span>Better to light one candle than to curse the darkness.</span>
+	<KDivider borderStyle="double" borderColor="b-pink"></KDivider>
+	<span>You don't have to see the whole staircase, just take the first step.</span>
+</div>
