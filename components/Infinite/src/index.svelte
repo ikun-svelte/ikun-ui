b361bd6f4ff7e6fb6d3eb7e0e9f5db32d7ef09f4
baiwusanyu-c <740132583@qq.com>
2023-09-19 19:23:43 +0800

    chore: updated code

--- components/Infinite/src/index.svelte
+++ components/Infinite/src/index.svelte
@@ -2,10 +2,23 @@
 	import { getPrefixCls } from '@ikun-ui/utils';
 	import { clsx } from 'clsx';
 	import type { KInfiniteProps } from './types';
-
+	import InfiniteScroll from 'svelte-infinite-scroll';
+	import { createEventDispatcher } from 'svelte';
 	export let cls: KInfiniteProps['cls'] = undefined;
 	export let attrs: KInfiniteProps['attrs'] = {};
 
+	export let threshold: KInfiniteProps['threshold'] = 0;
+	export let elementScroll: KInfiniteProps['elementScroll'] = undefined;
+	export let window: KInfiniteProps['window'] = false;
+	export let hasMore: KInfiniteProps['hasMore'] = true;
+	export let horizontal: KInfiniteProps['horizontal'] = false;
+	export let reverse: KInfiniteProps['reverse'] = false;
+
+	const dispatch = createEventDispatcher();
+	const handleLoadMore = () => {
+		dispatch('loadMore');
+	};
+	// class
 	const prefixCls = getPrefixCls('infinite');
 	$: cnames = clsx(
 		prefixCls,
@@ -16,4 +29,15 @@
 	);
 </script>
 
-<div class={cnames} {...$$restProps} {...attrs}></div>
+<div class={cnames} {...$$restProps} {...attrs}>
+	<slot />
+	<InfiniteScroll
+		{threshold}
+		{elementScroll}
+		{window}
+		{hasMore}
+		{horizontal}
+		{reverse}
+		on:loadMore={handleLoadMore}
+	/>
+</div>
