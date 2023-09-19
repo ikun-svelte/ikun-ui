d918f2899128efe6d2d7a2c90da39ea58197f4f9
baiwusanyu-c <740132583@qq.com>
2023-09-19 18:49:11 +0800

    chore:  infinite component init commit

---
+++ components/Infinite/src/index.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import { getPrefixCls } from '@ikun-ui/utils';
+	import { clsx } from 'clsx';
+	import type { KInfiniteProps } from './types';
+
+	export let cls: KInfiniteProps['cls'] = undefined;
+	export let attrs: KInfiniteProps['attrs'] = {};
+
+	const prefixCls = getPrefixCls('infinite');
+	$: cnames = clsx(
+		prefixCls,
+		{
+			[`${prefixCls}--base`]: true
+		},
+		cls
+	);
+</script>
+
+<div class={cnames} {...$$restProps} {...attrs}></div>
