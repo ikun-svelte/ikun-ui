61bd0b38d8989938afa4f28470d9d4596382774c
Victor Bo <hi@vtrbo.cn>
2023-09-27 12:04:18 +0800

    feat: added play toggle theme

---
+++ play/src/routes/+layout.svelte
@@ -0,0 +1,17 @@
+<script>
+	import { KIcon } from '@ikun-ui/icon';
+	const toggle = () => {
+		window.document.documentElement.classList.toggle('dark');
+	};
+</script>
+
+<header>
+	<div class="p4 fbc shadow color-ikun-black dark:(shadow-light-50 color-ikun-white)">
+		<div class="font-bold">IKun UI Play</div>
+		<KIcon class="i-carbon:sun dark:i-carbon:moon" on:click={toggle}></KIcon>
+	</div>
+</header>
+
+<main class="p4">
+	<slot />
+</main>
