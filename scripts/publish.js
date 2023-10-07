7c9bcd415cb221a6c32101880f3f052160968aac
baiwusanyu-c <740132583@qq.com>
2023-10-07 09:51:12 +0800

    chore: updated publish script

--- scripts/publish.js
+++ scripts/publish.js
@@ -1,7 +1,12 @@
 import { runTask } from './utils.js';
+async function doPublish() {
+	try {
+		await runTask('pnpm run publish:npm', 'components', 'publish');
+		await runTask('pnpm run publish:npm', 'preset', 'publish');
+		await runTask('pnpm run publish:npm', 'utils', 'publish');
+	} catch (e) {
+		console.error(e);
+	}
+}
 
-runTask('pnpm run publish:npm', 'components', 'publish').finally(() => {
-	runTask('pnpm run publish:npm', 'preset', 'publish').finally(() => {
-		runTask('pnpm run publish:npm', 'utils', 'publish');
-	});
-});
+doPublish();
