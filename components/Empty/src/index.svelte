b9d0b243a9e3f9325b9610e4a48a525b6d519281
baiwusanyu-c <740132583@qq.com>
2023-10-07 18:27:52 +0800

    chore: temp commit

--- components/Empty/src/index.svelte
+++ components/Empty/src/index.svelte
@@ -5,15 +5,46 @@
 
 	export let cls: KEmptyProps['cls'] = undefined;
 	export let attrs: KEmptyProps['attrs'] = {};
+  export let emptyDescription: KEmptyProps['emptyDescription'] = 'No Data';
+  export let image: KEmptyProps['image'] = '';
+  export let imageSize: KEmptyProps['imageSize'] = undefined
 
+  $: imgSize = imageSize ? `${imageSize}px` : imageSize
+
+  // class
 	const prefixCls = getPrefixCls('empty');
 	$: cnames = clsx(
 		prefixCls,
-		{
-			[`${prefixCls}--base`]: true
-		},
 		cls
 	);
+
+  $: imageCls = clsx(`${prefixCls}__image`);
+  $: imgCls = clsx(`${prefixCls}__img`);
+  $: descriptionCls = clsx(`${prefixCls}__description`);
+  $: descriptionPCls = clsx(`${prefixCls}__description__p`);
+  $: bottomCls = clsx(`${prefixCls}__bottom`);
 </script>
 
-<div class={cnames} {...$$restProps} {...attrs}></div>
+<div class={cnames} {...$$restProps} {...attrs}>
+  <div class="{imageCls}" style:width={imgSize}>
+    {#if image}
+      <img class="{imgCls}" src="{image}" on:dragstart={()=> false} alt=""/>
+    {:else }
+      <slot name="image">
+        <!--TODO: <img-empty />-->
+      </slot>
+    {/if}
+  </div>
+  <div class="{descriptionCls}">
+    {#if $$slots.description}
+      <slot name="description"></slot>
+    {:else }
+      <p class={descriptionPCls}> { emptyDescription }</p>
+    {/if}
+  </div>
+  {#if $$slots.default}
+    <div class="{bottomCls}">
+      <slot/>
+    </div>
+  {/if}
+</div>
---
+++ preset/src/shortcuts/src/empty.ts
@@ -0,0 +1,9 @@
+export const emptyShortcuts: Record<string, string> = {
+  // empty
+  'k-empty': 'fcc flex-col text-center box-border py-40 px-0',
+  'k-empty__image': 'w-160px',
+  'k-empty__img': 'w-full h-full select-none align-top object-contain',
+  'k-empty__description': 'mt-20px',
+  'k-empty__description__p': 'm-0 text-ikun-tx-gr text-sm',
+  'k-empty__bottom': 'mt-20px',
+};
