3f4e5e40a14c9e81bad25a6540256b9043e61d23
Victor Bo <hi@vtrbo.cn>
2023-10-09 11:31:20 +0800

    fix: fixed card component shadow effect is invalid in dark mode

--- preset/src/shortcuts/src/card.ts
+++ preset/src/shortcuts/src/card.ts
@@ -3,16 +3,16 @@ export const cardShortcuts = {
 	'k-card--header': 'p4 b-b-1px b-b-solid b-b-color-inherit',
 	'k-card--body': 'p4',
 
-	'k-card--transition': 'k-card--transition-timer',
+	'k-card--transition': 'k-card-transition-timer',
 
 	// shadow
-	'k-card--shadow__always': 'k-card--shadow',
+	'k-card--shadow__always': 'k-card-shadow',
 	'k-card--shadow__never': '',
-	'k-card--shadow__hover': 'hover:k-card--shadow focus:k-card--shadow',
+	'k-card--shadow__hover': 'hover:k-card-shadow focus:k-card-shadow',
 
 	// dark
 	'k-card__dark': 'dark:(bg-ikun-dark-300 b-ikun-dark text-ikun-white)',
-	'k-card--shadow__always__dark': 'dark:k-card--shadow__dark',
+	'k-card--shadow__always__dark': 'dark:k-card-shadow__dark',
 	'k-card--shadow__never__dark': '',
-	'k-card--shadow__hover__dark': 'dark:(hover:k-card--shadow__dark focus:k-card--shadow__dark)'
+	'k-card--shadow__hover__dark': 'dark:(hover:k-card-shadow__dark focus:k-card-shadow__dark)'
 };
