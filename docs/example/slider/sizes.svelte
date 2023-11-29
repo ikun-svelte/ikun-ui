346fb95a03d17e69390471286b60874973ddac25
Victor Bo <10667379@qq.com>
2023-11-29 18:01:11 +0800

    style: modify slider style

--- docs/example/slider/sizes.svelte
+++ docs/example/slider/sizes.svelte
@@ -2,8 +2,6 @@
 	import { KSlider } from '@ikun-ui/slider';
 </script>
 
-<div class="grid grid-cols-3 grid-gap-8px items-center">
-	<KSlider size="sm"></KSlider>
-	<KSlider size="md"></KSlider>
-	<KSlider size="lg"></KSlider>
-</div>
+<KSlider size="sm"></KSlider>
+<KSlider size="md"></KSlider>
+<KSlider size="lg"></KSlider>
