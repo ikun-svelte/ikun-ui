5493019ea2e80e562135061e43fddff0c90a3d8a
baiwusanyu-c <740132583@qq.com>
2023-11-30 18:03:21 +0800

    chore: init commit

---
+++ components/Watermark/src/index.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import { getPrefixCls } from '@ikun-ui/utils';
+	import { clsx } from 'clsx';
+	import type { KWatermarkProps } from './types';
+
+	export let cls: KWatermarkProps['cls'] = undefined;
+	export let attrs: KWatermarkProps['attrs'] = {};
+
+	const prefixCls = getPrefixCls('watermark');
+	$: cnames = clsx(
+		prefixCls,
+		{
+			[`${prefixCls}--base`]: true
+		},
+		cls
+	);
+</script>
+
+<div class={cnames} {...$$restProps} {...attrs}></div>
---
+++ components/Watermark/src/types.d.ts
@@ -0,0 +1,6 @@
+/// <reference types="svelte" />
+import type { ClassValue } from 'clsx';
+export type KWatermarkProps = {
+	cls: ClassValue;
+	attrs: Record<string, string>;
+};
