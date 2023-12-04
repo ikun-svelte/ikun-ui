831929d019615ac27520339dfba85d12ef700cfc
XiaoPan <2489964425@qq.com>
2023-12-04 17:49:05 +0800

    feat: slider component support vertical prop

--- preset/src/shortcuts/src/slider.ts
+++ preset/src/shortcuts/src/slider.ts
@@ -1,6 +1,7 @@
 export const sliderShortcuts: Record<string, string> = {
 	// slider
 	'k-slider--base': 'w-full fi',
+	'k-slider--base__vertical': 'h-full fc',
 
 	// disabled
 	'k-slider--disabled': 'k-bg-disabled',
@@ -9,16 +10,24 @@ export const sliderShortcuts: Record<string, string> = {
 	'k-slider--sm': 'h-24px',
 	'k-slider--md': 'h-32px',
 	'k-slider--lg': 'h-40px',
+	'k-slider--sm__vertical': 'w-24px',
+	'k-slider--md__vertical': 'w-32px',
+	'k-slider--lg__vertical': 'w-40px',
 
 	// runway
 	'k-slider--runway': 'flex-1 rd-0.75 bg-ikun-light-900 cursor-pointer pr',
+	'k-slider--runway__vertical': 'rd-0.75 bg-ikun-light-900 cursor-pointer pr fe flex-col',
 
 	// runway size [2px border]
 	'k-slider--runway--sm': 'mx-8px h-3px',
 	'k-slider--runway--md': 'mx-10px h-4px',
 	'k-slider--runway--lg': 'mx-12px h-5px',
+	'k-slider--runway--sm__vertical': 'my-8px w-3px',
+	'k-slider--runway--md__vertical': 'my-10px w-4px',
+	'k-slider--runway--lg__vertical': 'my-12px w-5px',
 
 	'k-slider--bar': 'h-full border-rd-0.75 bg-ikun-main',
+	'k-slider--bar__vertical': 'w-full border-rd-0.75 bg-ikun-main',
 
 	// runway disabled
 	'k-slider--runway__disabled': 'k-cur-disabled',
@@ -26,6 +35,8 @@ export const sliderShortcuts: Record<string, string> = {
 	// button
 	'k-slider--button-wrapper':
 		'fcc pa top-0 bottom-0 my-a translate-x--50% cursor-pointer select-none',
+	'k-slider--button-wrapper__vertical':
+		'fcc pa left-50% mx-a -translate-x-1/2 translate-y-1/2 cursor-pointer select-none',
 
 	// button wrapper size
 	'k-slider--button--sm': 'w-12px h-12px',
