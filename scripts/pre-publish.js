675ec28158f569b2e5d688ccf097338efa679db7
baiwusanyu-c <740132583@qq.com>
2023-12-22 15:50:51 +0800

    fix: fix missing exports condition

--- scripts/pre-publish.js
+++ scripts/pre-publish.js
@@ -4,4 +4,9 @@ const pathCWD = process.cwd();
 const packageJsonPath = path.join(pathCWD, 'package.json');
 const packageJson = fs.readJsonSync(packageJsonPath);
 packageJson.svelte = 'dist/index.js';
+packageJson.export = {
+	'.': {
+		svelte: './dist/index.js'
+	}
+};
 fs.writeJsonSync(packageJsonPath, packageJson, { spaces: 4 });
