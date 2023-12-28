f8a60d49753e9706e67b1980802f37cdcee5b6b7
baiwusanyu-c <740132583@qq.com>
2023-12-28 13:39:21 +0800

    chore: KImage component init commit

---
+++ components/Image/src/index.ts
@@ -0,0 +1,5 @@
+/// <reference types="./types" />
+import Image from './index.svelte';
+export { Image as KImage };
+
+export default Image;
---
+++ components/Image/src/types.d.ts
@@ -0,0 +1,6 @@
+/// <reference types="svelte" />
+import type { ClassValue } from 'clsx';
+export type KImageProps = {
+	cls: ClassValue;
+	attrs: Record<string, string>;
+};
