99b4c38f9d2a9d3f94f4f0b2274cc8501be2f8f3
baiwusanyu-c <740132583@qq.com>
2023-12-28 15:49:04 +0800

    chore: complete basic function

--- components/Image/src/types.d.ts
+++ components/Image/src/types.d.ts
@@ -1,6 +1,12 @@
 /// <reference types="svelte" />
 import type { ClassValue } from 'clsx';
 export type KImageProps = {
+	loading: 'eager' | 'lazy' | undefined
+	previewSrcList: string[]
+	fit: undefined | 'fill' | 'contain' | 'cover' | 'none' | 'scale-down' | string
+	lazy: boolean
+	src: string
+	alt: string
 	cls: ClassValue;
 	attrs: Record<string, string>;
 };
