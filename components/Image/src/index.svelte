5c25c2e8eccdc8598dab154414126aeaadac2108
baiwusanyu-c <740132583@qq.com>
2023-12-28 17:08:08 +0800

    chore: complete lazy load image

--- components/Image/src/index.svelte
+++ components/Image/src/index.svelte
@@ -2,137 +2,169 @@
 	import { getPrefixCls } from '@ikun-ui/utils';
 	import { clsx } from 'clsx';
 	import type { KImageProps } from './types';
-  import { BROWSER } from "esm-env";
-  import { createEventDispatcher, onMount } from "svelte";
-  export let previewSrcList: KImageProps['previewSrcList']  = []
-  export let fit: KImageProps['fit'] = undefined;
-  export let loading: KImageProps['loading'] = undefined;
-  export let lazy: KImageProps['lazy'] = false;
-  export let src: KImageProps['src'] = '';
-  export let alt: KImageProps['alt'] = '';
+	import { BROWSER } from 'esm-env';
+	import { createEventDispatcher, onMount, tick } from 'svelte';
+	import { isString, isElement, throttle } from 'baiwusanyu-utils';
+	import { getScrollContainer, isInContainer } from '@ikun-ui/utils';
+
+	export let scrollContainer: KImageProps['scrollContainer'] = undefined;
+	export let previewSrcList: KImageProps['previewSrcList'] = [];
+	export let fit: KImageProps['fit'] = undefined;
+	export let loading: KImageProps['loading'] = undefined;
+	export let lazy: KImageProps['lazy'] = false;
+	export let src: KImageProps['src'] = '';
+	export let alt: KImageProps['alt'] = '';
 	export let cls: KImageProps['cls'] = undefined;
 	export let attrs: KImageProps['attrs'] = {};
 
-  let imageSrc: string | undefined = undefined
-  let hasLoadError = false
-  let isLoading = true
+	let imageSrc: string | undefined = undefined;
+	let hasLoadError = false;
+	let isLoading = true;
 
-  const dispatch = createEventDispatcher();
-  const loadImage = () => {
-    if (!BROWSER) return
-    // reset status
-    isLoading = true
-    hasLoadError = false
-    imageSrc = src
-  }
+	const dispatch = createEventDispatcher();
+	const loadImage = () => {
+		if (!BROWSER) return;
+		// reset status
+		isLoading = true;
+		hasLoadError = false;
+		imageSrc = src;
+	};
 
-  function handleLoad(event: Event) {
-    isLoading = false
-    hasLoadError = false
-    dispatch('load', event)
-  }
+	function handleLoad(event: Event) {
+		isLoading = false;
+		hasLoadError = false;
+		dispatch('load', event);
+	}
 
-  function handleError(event: Event) {
-    isLoading = false
-    hasLoadError = true
-    dispatch('error', event)
-  }
+	function handleError(event: Event) {
+		isLoading = false;
+		hasLoadError = true;
+		dispatch('error', event);
+	}
 
-  const supportLoading = BROWSER && 'loading' in HTMLImageElement.prototype
+	let container: HTMLElement | undefined = undefined;
+	let _scrollContainer: HTMLElement | Window | undefined = undefined;
+	function handleLazyLoad() {
+		if (isInContainer(container, _scrollContainer)) {
+			loadImage();
+			removeLazyLoadListener();
+		}
+	}
 
-  $: isManual = function(loadingValue, lazy){
-    if (loadingValue === 'eager') return false
-    return (!supportLoading && loadingValue === 'lazy') || lazy
-  }(loading, lazy)
+	const lazyLoadHandler = throttle(handleLazyLoad, 200);
 
-  let oldSrc = src
-  $: {
-    if(oldSrc !== src){
-      if (isManual) {
-        // reset status
-        // isLoading = true
-        // hasLoadError = false
-        // removeLazyLoadListener()
-        // addLazyLoadListener()
-      } else {
-        loadImage()
-      }
-      oldSrc = src
-    }
+	async function addLazyLoadListener() {
+		if (!BROWSER) return;
 
-  }
+		await tick();
 
-  onMount(() => {
-    if (isManual) {
-      // addLazyLoadListener()
-    } else {
-      loadImage()
-    }
-  })
+		if (isElement(scrollContainer)) {
+			_scrollContainer = scrollContainer as HTMLElement;
+		} else if (isString(scrollContainer) && scrollContainer !== '') {
+			_scrollContainer =
+				document.querySelector<HTMLElement>(scrollContainer as string) ?? undefined;
+		} else if (container) {
+			_scrollContainer = getScrollContainer(container);
+		}
+		if (_scrollContainer) {
+			_scrollContainer.addEventListener('scroll', lazyLoadHandler);
+			setTimeout(() => handleLazyLoad(), 100);
+		}
+	}
 
+	function removeLazyLoadListener() {
+		if (!BROWSER || !_scrollContainer || !lazyLoadHandler) return;
 
-  function clickHandler() {
-    // don't show viewer when preview is false
-    //if (!preview.value) return
-    //
-    //stopWheelListener = useEventListener('wheel', wheelHandler, {
-    //  passive: false,
-    //})
-    //
-    //// prevent body scroll
-    //prevOverflow = document.body.style.overflow
-    //document.body.style.overflow = 'hidden'
-    //showViewer.value = true
-    //emit('show')
-  }
+		_scrollContainer && _scrollContainer.removeEventListener('scroll', lazyLoadHandler);
+		_scrollContainer = undefined;
+	}
 
-  $: isPreview = Array.isArray(previewSrcList) && previewSrcList.length > 0
+	const supportLoading = BROWSER && 'loading' in HTMLImageElement.prototype;
+	$: isManual = (function (loadingValue, lazy) {
+		if (loadingValue === 'eager') return false;
+		return (!supportLoading && loadingValue === 'lazy') || lazy;
+	})(loading, lazy);
+
+	let oldSrc = src;
+	$: {
+		if (oldSrc !== src) {
+			if (isManual) {
+				// reset status
+				isLoading = true;
+				hasLoadError = false;
+				removeLazyLoadListener();
+				addLazyLoadListener();
+			} else {
+				loadImage();
+			}
+			oldSrc = src;
+		}
+	}
+
+	onMount(() => {
+		if (isManual) {
+			addLazyLoadListener();
+		} else {
+			loadImage();
+		}
+	});
+
+	function clickHandler() {
+		// don't show viewer when preview is false
+		//if (!preview.value) return
+		//
+		//stopWheelListener = useEventListener('wheel', wheelHandler, {
+		//  passive: false,
+		//})
+		//
+		//// prevent body scroll
+		//prevOverflow = document.body.style.overflow
+		//document.body.style.overflow = 'hidden'
+		//showViewer.value = true
+		//emit('show')
+	}
+
+	$: isPreview = Array.isArray(previewSrcList) && previewSrcList.length > 0;
 
 	const prefixCls = getPrefixCls('image');
-	$: cnames = clsx(
-		prefixCls,
-		cls
-	);
-  $: errorCls = clsx(`${prefixCls}__error`)
-  $: wrapperCls = clsx(`${prefixCls}__wrapper`)
-  $: placeholderCls = clsx(`${prefixCls}__placeholder`)
-  $: imageKls = clsx(
-    `${prefixCls}__inner`,
-    {
-      [`${prefixCls}__inner`]: isPreview,
-      [`${prefixCls}__loading`]: isLoading
-    }
-  )
+	$: cnames = clsx(prefixCls, cls);
+	$: errorCls = clsx(`${prefixCls}__error`);
+	$: wrapperCls = clsx(`${prefixCls}__wrapper`);
+	$: placeholderCls = clsx(`${prefixCls}__placeholder`);
+	$: imageKls = clsx(`${prefixCls}__inner`, {
+		[`${prefixCls}__inner`]: isPreview,
+		[`${prefixCls}__loading`]: isLoading
+	});
 </script>
 
-<div class={cnames}>
-  {#if hasLoadError}
-    <slot name="error">
-      <!--TODO: i18n -->
-      <div class={errorCls}>FAILED</div>
-    </slot>
-  {:else }
-    {#if imageSrc !== undefined}
-      <img
-        alt={alt}
-        aria-hidden="true"
-        {...$$restProps}
-        {...attrs}
-        src={imageSrc}
-        loading="{loading}"
-        style:object-fit="{fit}"
-        class="{imageKls}"
-        on:click="{clickHandler}"
-        on:load="{handleLoad}"
-        on:error="{handleError}"
-      />
-    {/if}
-    {#if isLoading}
-      <div class={wrapperCls}>
-        <slot name="placeholder">
-          <div class={placeholderCls} />
-        </slot>
-      </div>
-    {/if}
-  {/if}
+<div class={cnames} bind:this={container}>
+	{#if hasLoadError}
+		<slot name="error">
+			<!--TODO: i18n -->
+			<div class={errorCls}>FAILED</div>
+		</slot>
+	{:else}
+		{#if imageSrc !== undefined}
+			<img
+				{alt}
+				aria-hidden="true"
+				{...$$restProps}
+				{...attrs}
+				src={imageSrc}
+				{loading}
+				style:object-fit={fit}
+				class={imageKls}
+				on:click={clickHandler}
+				on:load={handleLoad}
+				on:error={handleError}
+			/>
+		{/if}
+		{#if isLoading}
+			<div class={wrapperCls}>
+				<slot name="placeholder">
+					<div class={placeholderCls} />
+				</slot>
+			</div>
+		{/if}
+	{/if}
 </div>
--- components/Image/src/types.d.ts
+++ components/Image/src/types.d.ts
@@ -1,12 +1,13 @@
 /// <reference types="svelte" />
 import type { ClassValue } from 'clsx';
 export type KImageProps = {
-	loading: 'eager' | 'lazy' | undefined
-	previewSrcList: string[]
-	fit: undefined | 'fill' | 'contain' | 'cover' | 'none' | 'scale-down' | string
-	lazy: boolean
-	src: string
-	alt: string
+	scrollContainer: string | HTMLElement | undefined;
+	loading: 'eager' | 'lazy' | undefined;
+	previewSrcList: string[];
+	fit: undefined | 'fill' | 'contain' | 'cover' | 'none' | 'scale-down' | string;
+	lazy: boolean;
+	src: string;
+	alt: string;
 	cls: ClassValue;
 	attrs: Record<string, string>;
 };
