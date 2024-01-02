8aa3d5cddab5129b8989d7512dcaf34ac78b453b
baiwusanyu-c <740132583@qq.com>
2024-01-02 11:40:11 +0800

    test: added KView component unit test

---
+++ components/Image/__test__/fixture/placeholder.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import { KImage } from '@ikun-ui/image';
+	const url = '';
+</script>
+
+<KImage style="width: 100px; height: 100px" src={url}>
+	<p slot="placeholder">test placeholder</p>
+</KImage>
