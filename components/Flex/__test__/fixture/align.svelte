1432c41ecad66339f69e265dc1693276d4636698
白雾三语 <32354856+baiwusanyu-c@users.noreply.github.com>
2024-01-04 09:29:52 +0800

    test: added KFlex unit test (#388)

---
+++ components/Flex/__test__/fixture/align.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import { KFlex } from '@ikun-ui/flex';
+</script>
+
+<KFlex align="normal"></KFlex>
+<KFlex align="start"></KFlex>
+<KFlex align="end"></KFlex>
+<KFlex align="center"></KFlex>
+<KFlex align="baseline"></KFlex>
+<KFlex align="stretch"></KFlex>
---
+++ components/Flex/__test__/fixture/gap.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import { KFlex } from '@ikun-ui/flex';
+</script>
+
+<KFlex gap={90}></KFlex>
+<KFlex gap="md"></KFlex>
+<KFlex gap="sm"></KFlex>
+<KFlex gap="lg"></KFlex>
