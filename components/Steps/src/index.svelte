9ff812dbab7c393fe7f371c3e9e0acf6b073f24a
baiwusanyu-c <740132583@qq.com>
2024-01-11 14:10:02 +0800

    chore: complete KStep slot

--- components/Steps/src/index.svelte
+++ components/Steps/src/index.svelte
@@ -1,10 +1,9 @@
 <script lang="ts">
 	import { getPrefixCls } from '@ikun-ui/utils';
 	import { clsx } from 'clsx';
-	import type { KStepsItem, KStepsProps } from './types';
+	import type { KStepsProps } from './types';
 	import KStep from './step.svelte';
 	import { createEventDispatcher } from 'svelte';
-	import { scale } from 'svelte/transition';
 	export let active: KStepsProps['active'] = 0;
 	export let direction: KStepsProps['direction'] = 'horizontal';
 	export let labelPlacement: KStepsProps['labelPlacement'] = 'horizontal';
@@ -16,11 +15,11 @@
 	export let attrs: KStepsProps['attrs'] = {};
 
 	const dispatch = createEventDispatcher();
-	function handleChange(item: KStepsItem) {
+	function handleChange(e: CustomEvent) {
 		if (!canClick) return;
-		dispatch('change', item);
+		dispatch('change', e.detail);
 	}
-	$: isActive = (index: number) => Number(active) === Number(index);
+
 	const prefixCls = getPrefixCls('steps');
 	$: cnames = clsx(
 		prefixCls,
@@ -29,52 +28,24 @@
 		},
 		cls
 	);
-	$: containerCls = clsx(`${prefixCls}-container`, {
-		[`${prefixCls}-label-container-vertical`]:
-			labelPlacement === 'vertical' || direction === 'vertical',
-		[`${prefixCls}-container-${direction}`]: !navigation || (dot && navigation),
-		[`${prefixCls}-container-horizontal--nav`]: !dot && navigation && direction === 'horizontal'
-	});
-	$: barCls = clsx({
-		[`${prefixCls}-bar-${direction}`]: !dot && navigation && labelPlacement === 'horizontal',
-		[`${prefixCls}-bar-lb-horizontal`]:
-			!dot && navigation && labelPlacement === 'vertical' && direction === 'horizontal'
-	});
 </script>
 
 <div class={cnames} {...$$restProps} {...attrs}>
 	<slot>
 		{#each items as item, index}
-			<div class={containerCls} aria-hidden="true" data-k-step on:click={() => handleChange(item)}>
-				<KStep
-					{item}
-					eachIndex={index + 1}
-					{active}
-					{labelPlacement}
-					{direction}
-					{canClick}
-					{dot}
-					{navigation}
-					last={index === items.length - 1}
-					index={item.index}
-				></KStep>
-
-				{#if !dot && navigation && isActive(index + 1) && direction === 'horizontal'}
-					<div
-						class={barCls}
-						out:scale={{ duration: 300, start: 0.3 }}
-						in:scale={{ duration: 300, start: 0.3 }}
-					></div>
-				{/if}
-
-				{#if !dot && navigation && isActive(index + 1) && direction === 'vertical'}
-					<div
-						class={barCls}
-						out:scale={{ duration: 300, start: 0.3 }}
-						in:scale={{ duration: 300, start: 0.3 }}
-					></div>
-				{/if}
-			</div>
+			<KStep
+				on:change={handleChange}
+				{item}
+				eachIndex={index + 1}
+				{active}
+				{labelPlacement}
+				{direction}
+				{canClick}
+				{dot}
+				{navigation}
+				last={index === items.length - 1}
+				index={item.index}
+			></KStep>
 		{/each}
 	</slot>
 </div>
