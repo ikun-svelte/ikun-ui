7ffdb424609a02303ff700e26e80d20bc4460b4c
baiwusanyu-c <740132583@qq.com>
2024-01-11 13:15:33 +0800

    chore: complete navigation prop

--- components/Steps/src/index.svelte
+++ components/Steps/src/index.svelte
@@ -1,25 +1,26 @@
 <script lang="ts">
 	import { getPrefixCls } from '@ikun-ui/utils';
 	import { clsx } from 'clsx';
-	import type { KStepsOption, KStepsProps } from './types';
+	import type { KStepsItem, KStepsProps } from './types';
 	import KStep from './step.svelte';
 	import { createEventDispatcher } from 'svelte';
+	import { scale } from 'svelte/transition';
 	export let active: KStepsProps['active'] = 0;
 	export let direction: KStepsProps['direction'] = 'horizontal';
 	export let labelPlacement: KStepsProps['labelPlacement'] = 'horizontal';
 	export let dot: KStepsProps['dot'] = false;
 	export let navigation: KStepsProps['navigation'] = false;
-	export let options: KStepsProps['options'] = [];
+	export let items: KStepsProps['items'] = [];
 	export let canClick: KStepsProps['canClick'] = false;
 	export let cls: KStepsProps['cls'] = undefined;
 	export let attrs: KStepsProps['attrs'] = {};
 
 	const dispatch = createEventDispatcher();
-	function handleChange(option: KStepsOption) {
+	function handleChange(item: KStepsItem) {
 		if (!canClick) return;
-		dispatch('change', option);
+		dispatch('change', item);
 	}
-
+	$: isActive = (index: number) => Number(active) === Number(index);
 	const prefixCls = getPrefixCls('steps');
 	$: cnames = clsx(
 		prefixCls,
@@ -28,32 +29,51 @@
 		},
 		cls
 	);
-	$: containerCls = clsx(`${prefixCls}-container`, `${prefixCls}-container-${direction}`, {
+	$: containerCls = clsx(`${prefixCls}-container`, {
 		[`${prefixCls}-label-container-vertical`]:
-			labelPlacement === 'vertical' || direction === 'vertical'
+			labelPlacement === 'vertical' || direction === 'vertical',
+		[`${prefixCls}-container-${direction}`]: !navigation || (dot && navigation),
+		[`${prefixCls}-container-horizontal--nav`]: !dot && navigation && direction === 'horizontal'
+	});
+	$: barCls = clsx({
+		[`${prefixCls}-bar-${direction}`]: !dot && navigation && labelPlacement === 'horizontal',
+		[`${prefixCls}-bar-lb-horizontal`]:
+			!dot && navigation && labelPlacement === 'vertical' && direction === 'horizontal'
 	});
 </script>
 
 <div class={cnames} {...$$restProps} {...attrs}>
 	<slot>
-		{#each options as option, index}
-			<div
-				class={containerCls}
-				aria-hidden="true"
-				data-k-step
-				on:click={() => handleChange(option)}
-			>
+		{#each items as item, index}
+			<div class={containerCls} aria-hidden="true" data-k-step on:click={() => handleChange(item)}>
 				<KStep
-					{option}
+					{item}
 					eachIndex={index + 1}
 					{active}
 					{labelPlacement}
 					{direction}
 					{canClick}
 					{dot}
-					last={index === options.length - 1}
-					index={option.index}
+					{navigation}
+					last={index === items.length - 1}
+					index={item.index}
 				></KStep>
+
+				{#if !dot && navigation && isActive(index + 1) && direction === 'horizontal'}
+					<div
+						class={barCls}
+						out:scale={{ duration: 300, start: 0.3 }}
+						in:scale={{ duration: 300, start: 0.3 }}
+					></div>
+				{/if}
+
+				{#if !dot && navigation && isActive(index + 1) && direction === 'vertical'}
+					<div
+						class={barCls}
+						out:scale={{ duration: 300, start: 0.3 }}
+						in:scale={{ duration: 300, start: 0.3 }}
+					></div>
+				{/if}
 			</div>
 		{/each}
 	</slot>
