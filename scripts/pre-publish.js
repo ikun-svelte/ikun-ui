ea5a15a81033ccc9c85217ef440be0a9780e3be4
baiwusanyu-c <740132583@qq.com>
2024-03-04 14:42:20 +0800

    fix: delete svelte attr in pkg json

--- scripts/pre-publish.js
+++ scripts/pre-publish.js
@@ -3,10 +3,12 @@ import fs from 'fs-extra';
 const pathCWD = process.cwd();
 const packageJsonPath = path.join(pathCWD, 'package.json');
 const packageJson = fs.readJsonSync(packageJsonPath);
-packageJson.svelte = 'dist/index.js';
+
 packageJson.export = {
 	'.': {
 		svelte: './dist/index.js'
 	}
 };
+
+Reflect.deleteProperty(packageJson, 'svelte')
 fs.writeJsonSync(packageJsonPath, packageJson, { spaces: 4 });
