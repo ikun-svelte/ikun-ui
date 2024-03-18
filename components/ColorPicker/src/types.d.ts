42825362503a930f41b83039f511ec64e3b305bd
baiwusanyu-c <740132583@qq.com>
2024-03-18 16:45:34 +0800

    chore: updated todo

--- components/ColorPicker/src/types.d.ts
+++ components/ColorPicker/src/types.d.ts
@@ -179,7 +179,7 @@ export type KColorPickerFormatProps = {
 // TODO: 🚧 props disabledAlpha 禁用透明度 boolean @default false
 // TODO: 🚧 props placement unit test
 // TODO: 🚧 props presets 预设的颜色  { label: string, colors: Array<string | Color>, defaultOpen?: boolean }[] @default undefined
-// TODO: props size 触发器大小 IkunSize @default 'md'
+// TODO: 🚧 props size 触发器大小 IkunSize @default 'md'
 // TODO: 🚧 props showText 触发器是否显示文本 boolean @default false
 // TODO: 🚧 props trigger unit test
 
