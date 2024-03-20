06dd9a5166d872503b3c24f1270d0fff4a3c25df
白雾三语 <32354856+baiwusanyu-c@users.noreply.github.com>
2024-03-20 21:43:50 +0800

    feat: color picker component adds tinycolor2 api (#420)

--- components/ColorPicker/src/index.ts
+++ components/ColorPicker/src/index.ts
@@ -5,6 +5,8 @@ import ColorPickerSlider from './slider.svelte';
 import ColorPickerBlock from './block.svelte';
 import ColorPickerFormat from './format.svelte';
 import ColorPickerPreset from './preset.svelte';
+import tinycolor from 'tinycolor2';
+import type { ColorInput } from 'tinycolor2';
 export { ColorPicker as KColorPicker };
 export { ColorPickerPalette as KColorPickerPalette };
 export { ColorPickerSlider as KColorPickerSlider };
@@ -21,3 +23,28 @@ export type {
 	KColorPickerFormatProps,
 	KColorPickerPresetProps
 } from './types';
+
+export type Color = ColorInput;
+export function toHex(value: Color) {
+	return tinycolor(value).toHex();
+}
+
+export function toHexString(value: Color) {
+	return tinycolor(value).toHexString();
+}
+
+export function toHsv(value: Color) {
+	return tinycolor(value).toHsv();
+}
+
+export function toHsbString(value: Color) {
+	return tinycolor(value).toHsvString();
+}
+
+export function toRgb(value: Color) {
+	return tinycolor(value).toRgb();
+}
+
+export function toRgbString(value: Color) {
+	return tinycolor(value).toRgbString();
+}
