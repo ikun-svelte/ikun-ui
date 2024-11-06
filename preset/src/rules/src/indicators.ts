f2e3951565a604d554955403e59d786c1b92eb86
Onion-L <lixiang020317@gmail.com>
2024-11-06 21:22:36 +0800

    feat: add vertical layout support for KCarousel

---
+++ preset/src/rules/src/indicators.ts
@@ -0,0 +1,3 @@
+export const indicatorsRules = {
+	'k-indicators-tanslate-y-half': { transform: 'translateY(-50%)' }
+};
--- preset/src/shortcuts/src/indicators.ts
+++ preset/src/shortcuts/src/indicators.ts
@@ -1,6 +1,9 @@
 export const indicatorsShortcuts: Record<string, string> = {
 	'k-indicators': 'pa w-full fc bottom-12px',
+	'k-indicators--vertical':
+		'flex-col items-end h-70px justify-around top-50% k-indicators-tanslate-y-half',
 	'k-indicators-item':
 		'h-6px w-6px rounded-full mr-8px bg-ikun-bg-grs cursor-pointer k-carousel-item',
-	'k-indicators-item--active': 'bg-ikun-main w-24px k-carousel-item'
+	'k-indicators-item--active__horizontal': 'bg-ikun-main w-24px k-carousel-item',
+	'k-indicators-item--active__vertical': 'bg-ikun-main h-18px k-carousel-item'
 };
